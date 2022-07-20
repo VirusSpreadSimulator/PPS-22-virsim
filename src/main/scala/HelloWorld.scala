9365ee3549dae58417e5d0e86d235b6bb32ad568
Andrea Giulianelli <andrygiulio@gmail.com>
2022-07-20 16:58:47 +0200

    docs: add scala doc for testing purposes

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,4 +1,9 @@
 object HelloWorld:
   @main def hello(): Unit =
     print("ciao")
+
+  /** Get the project name
+    * @return
+    *   the project name
+    */
   def getProjectName: String = "virsim"
