6527bf040149489a892d3bb32421187e9e869af1
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-01 12:26:12 +0200

    chore: added time concept

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/common/SpaceTime.scala
@@ -0,0 +1,17 @@
+package it.unibo.pps.entity.common
+
+import scala.concurrent.duration
+import scala.concurrent.duration.{FiniteDuration, TimeUnit}
+
+object SpaceTime:
+  /** Alias for finite duration time */
+  type DurationTime = FiniteDuration
+  /** Factory for [[DurationTime]]
+    * @param length
+    *   the length of the duration
+    * @param unit
+    *   the unit used to express the duration
+    * @return
+    *   [[DurationTime]] instance
+    */
+  def DurationTime(length: Long, unit: TimeUnit): DurationTime = FiniteDuration(lenght, unit)
---
+++ root/shared/src/test/scala/it/unibo/pps/common/SpaceTimeTests.scala
@@ -0,0 +1,13 @@
+package it.unibo.pps.common
+
+import it.unibo.pps.entity.common.SpaceTime.DurationTime
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class SpaceTimeTests extends AnyFunSuite with Matchers:
+  test("It's possible to obtain length and unit from a duration") {
+    import scala.concurrent.duration.MILLISECONDS
+    val duration = DurationTime(100, MILLISECONDS)
+    duration.length shouldBe 100
+    duration.unit shouldBe MILLISECONDS
+  }
