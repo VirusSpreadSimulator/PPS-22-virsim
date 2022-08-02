70327597a23b2b85c0d1b65292d54f121a9d5ae1
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-02 17:22:14 +0200

    test: red test timestamp

--- root/shared/src/test/scala/it/unibo/pps/common/TimeTests.scala
+++ root/shared/src/test/scala/it/unibo/pps/common/TimeTests.scala
@@ -1,13 +1,22 @@
 package it.unibo.pps.common
 
 import it.unibo.pps.entity.common.Time.DurationTime
+import it.unibo.pps.entity.common.Time.TimeStamp
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class TimeTests extends AnyFunSuite with Matchers:
+  private val time = 100
+
   test("It's possible to obtain length and unit from a duration") {
     import scala.concurrent.duration.MILLISECONDS
-    val duration = DurationTime(100, MILLISECONDS)
-    duration.length shouldBe 100
+    val duration = DurationTime(time, MILLISECONDS)
+    duration.length shouldBe time
     duration.unit shouldBe MILLISECONDS
+  }
+
+  test("A timestamp should be convertible to its absolute value respect the start of the simulation") {
+    val iterationNumber = 2
+    val timeStamp = TimeStamp(time, iterationNumber)
+    timeStamp.toAbsoluteValue() shouldBe (time * iterationNumber)
   }
