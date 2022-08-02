e1994fcfbf6b7b37f3278ac185057258699ac525
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-03 00:41:52 +0200

    feat(enity): add basic implementation of virus

--- root/shared/src/main/scala/it/unibo/pps/entity/virus/VirusComponent.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/virus/VirusComponent.scala
@@ -2,28 +2,43 @@ package it.unibo.pps.entity.virus
 
 object VirusComponent:
 
-  trait Virus:
+  trait VirusConfiguration:
 
     /** The name of the virus inside the environment.
       * @return
       *   the name of the virus.
       */
-    def name: String
+    def getVirusName: Option[String]
 
     /** The ability of the virus to spread inside the population.
       * @return
       *   the spread rate.
       */
-    def spreadRate: Double
+    def getSpreadRate: Option[Double]
 
     /** The average duration of the virus infection.
       * @return
       *   the average days of infection.
       */
-    def averagePositivityDays: Int
+    def getAveragePositivityDays: Option[Int]
 
     /** The ability of the virus to cause health damages.
       * @return
       *   the desease probability of the virus.
       */
-    def severeDeseaseProbability: Int
+    def getSevereDeseaseProbability: Option[Int]
+
+  class Virus extends VirusConfiguration:
+
+    var name: Option[String] = Some("Generic-Virus")
+    var spreadRate: Option[Double] = Some(1.5)
+    var averagePositivityDays: Option[Int] = Some(7)
+    var severeDeseaseProbability: Option[Int] = Some(35)
+
+    override def getVirusName: Option[String] = name
+
+    override def getSpreadRate: Option[Double] = spreadRate
+
+    override def getAveragePositivityDays: Option[Int] = averagePositivityDays
+
+    override def getSevereDeseaseProbability: Option[Int] = severeDeseaseProbability
