40f6a474cb8c319147dacc6ef3339f54eb29d3ce
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-02 23:43:09 +0200

    feat(dsl): add basic dsl for simulation configuration

---
+++ root/shared/src/main/scala/it/unibo/pps/control/loader/configuration/ConfigurationDSL.scala
@@ -0,0 +1,26 @@
+package it.unibo.pps.control.loader.configuration
+
+import it.unibo.pps.control.loader.configuration.ConfigurationComponent.VirsimConfiguration
+import it.unibo.pps.control.engine.SimulationComponent.Simulation
+
+object ConfigurationDSL:
+
+  extension (simulation: Simulation)
+    def days(numberOfDays: Int): Simulation =
+      simulation.duration = Some(numberOfDays)
+      simulation
+    def entities(numberOfEntities: Int): Simulation =
+      simulation.numberOfEntities = Some(numberOfEntities)
+      simulation
+    def averagePopulationAge(age: Int): Simulation =
+      simulation.averagePopulationAge = Some(age)
+      simulation
+    def stdDevPopulationAge(standardDeviation: Double): Simulation =
+      simulation.stdDevPopulationAge = Some(standardDeviation)
+      simulation
+    def peoplePerHouse(numberOfPeople: Int): Simulation =
+      simulation.peoplePerHouse = Some(numberOfPeople)
+      simulation
+    def startingInfectedPercentage(infectedPeople: Int): Simulation =
+      simulation.startingInfectedPercentage = Some(infectedPeople)
+      simulation
