f556757e72138cfa13b3201f113cae03b381ac48
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-02 17:45:47 +0200

    chore(entity): add virus component

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/virus/VirusComponent.scala
@@ -0,0 +1,23 @@
+package it.unibo.pps.entity.virus
+
+object VirusComponent:
+
+  trait Virus:
+
+    /** The ability of the virus to spread inside the population.
+      * @return
+      *   the spread rate.
+      */
+    def spreadRate: Double
+
+    /** The average duration of the virus infection.
+      * @return
+      *   the average days of infection.
+      */
+    def averagePositivityDays: Int
+
+    /** The ability of the virus to cause health damages.
+      * @return
+      *   the desease probability of the virus.
+      */
+    def severeDeseaseProbability: Int
