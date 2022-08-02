6f1abdbf2b739294707892e281185bff2e33e28a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-02 17:45:11 +0200

    chore(loading): add configuration component

---
+++ root/shared/src/main/scala/it/unibo/pps/control/loader/configuration/ConfigurationComponent.scala
@@ -0,0 +1,33 @@
+package it.unibo.pps.control.loader.configuration
+
+import it.unibo.pps.control.loader.configuration.SimulationConfigurations.SimulationConfiguration
+import it.unibo.pps.control.loader.configuration.VirusConfigurations.VirusConfiguration
+import it.unibo.pps.entity.structure.StructureComponent.Structure
+
+object ConfigurationComponent:
+
+  trait Configuration:
+
+    /** The section of the configuration file about the simulation parameters.
+      * @return
+      *   a configuration of the simulation.
+      */
+    def simulationConfiguration: SimulationConfiguration
+
+    /** The section of the configuration file about the virus parameters.
+      * @return
+      *   a configuration of the virus.
+      */
+    def virusConfiguration: VirusConfiguration
+
+    /** The section of the configuration file about the structures parameters.
+      * @return
+      *   a configuration of the structures.
+      */
+    def structuresConfiguration: Set[Structure]
+
+  case class VirsimConfiguration(
+      override val simulationConfiguration: SimulationConfiguration,
+      override val virusConfiguration: VirusConfiguration,
+      override val structuresConfiguration: Set[Structure]
+  ) extends Configuration
