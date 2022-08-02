0f04b69396592095a0fcc899751f07fe1b194683
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-02 16:48:07 +0200

    style: refactor structures tests

--- root/shared/src/test/scala/it/unibo/pps/structure/entrance/StructuresTest.scala
+++ root/shared/src/test/scala/it/unibo/pps/structure/entrance/StructuresTest.scala
@@ -17,99 +17,75 @@ class StructuresTest extends AnyFunSuite with Matchers:
   private val entities = Seq("entity1", "entity2", "entity3")
   private val position = Point2D(3, 0)
   private val treatmentQuality = Hospitalization.TreatmentQuality.GOOD
+  private val house = House(infectionProbability, capacity)
+  private val building = GenericBuilding(infectionProbability, capacity, position = position)
+  private val hospital =
+    Hospital(infectionProbability, capacity, position = position, treatmentQuality = treatmentQuality)
 
   test("Initially a house is empty") {
-    val house: Structure = House(infectionProbability, capacity)
     house.entities.size shouldBe 0
   }
 
   test("In a house can enter anyone") {
-    var house: Structure = House(infectionProbability, capacity)
-    house = house.tryToEnter(entities.head)
-    house.entities.size shouldBe 1
+    var houseCopy: Structure = house.copy()
+    houseCopy = houseCopy.tryToEnter(entities.head)
+    houseCopy.entities.size shouldBe 1
   }
 
   test("No more entities than capacity can enter in a House") {
-    var house: Structure = House(infectionProbability, capacity)
-    house = tryToEnterMultiple(house, entities)
-    house.entities.size shouldBe capacity
+    var houseCopy: Structure = house.copy()
+    houseCopy = tryToEnterMultiple(houseCopy, entities)
+    houseCopy.entities.size shouldBe capacity
   }
 
   test("Initially a generic building is empty") {
-    val building: Structure = GenericBuilding(infectionProbability, capacity, position = position)
     building.entities.size shouldBe 0
   }
 
   test("In a generic building the strategy must be considered, true case") {
-    var building: Structure =
-      GenericBuilding(infectionProbability, capacity, entranceStrategy = FilteredStrategyTrue(), position = position)
-    building = building.tryToEnter(entities.head)
-    building.entities.size shouldBe 1
+    var buildingCopy: Structure = building.copy(entranceStrategy = FilteredStrategyTrue())
+    buildingCopy = buildingCopy.tryToEnter(entities.head)
+    buildingCopy.entities.size shouldBe 1
   }
 
   test("In a generic building the strategy must be considered, false case") {
-    var building: Structure =
-      GenericBuilding(infectionProbability, capacity, entranceStrategy = FilteredStrategyFalse(), position = position)
-    building = building.tryToEnter(entities.head)
-    building.entities.size shouldBe 0
+    var buildingCopy: Structure = building.copy(entranceStrategy = FilteredStrategyFalse())
+    buildingCopy = buildingCopy.tryToEnter(entities.head)
+    buildingCopy.entities.size shouldBe 0
   }
 
   test("A generic building can be closed after creation and will not accept entities") {
-    var building = GenericBuilding(infectionProbability, capacity, position = position)
-    building = building.focus(_.isOpen).replace(false)
-    building.tryToEnter(entities.head).entities.size shouldBe 0
+    var buildingCopy = building.copy()
+    buildingCopy = buildingCopy.focus(_.isOpen).replace(false)
+    buildingCopy.tryToEnter(entities.head).entities.size shouldBe 0
   }
 
   test("In a generic building even if the strategy allow the entity, the capacity must be respected") {
-    var building: Structure =
-      GenericBuilding(infectionProbability, capacity, entranceStrategy = FilteredStrategyTrue(), position = position)
-    building = tryToEnterMultiple(building, entities)
-    building.entities.size shouldBe capacity
+    var buildingCopy: Structure = building.copy(entranceStrategy = FilteredStrategyTrue())
+    buildingCopy = tryToEnterMultiple(buildingCopy, entities)
+    buildingCopy.entities.size shouldBe capacity
   }
 
   test("Initially a hospital is empty") {
-    val hospital: Structure =
-      Hospital(infectionProbability, capacity, position = position, treatmentQuality = treatmentQuality)
     hospital.entities.size shouldBe 0
   }
 
   test("In a hospital the strategy must be considered, true case") {
-    var hospital: Structure =
-      Hospital(
-        infectionProbability,
-        capacity,
-        entranceStrategy = FilteredStrategyTrue(),
-        position = position,
-        treatmentQuality = treatmentQuality
-      )
-    hospital = hospital.tryToEnter(entities.head)
-    hospital.entities.size shouldBe 1
+    var hospitalCopy: Structure = hospital.copy(entranceStrategy = FilteredStrategyTrue())
+    hospitalCopy = hospitalCopy.tryToEnter(entities.head)
+    hospitalCopy.entities.size shouldBe 1
   }
 
   test("In a hospital the strategy must be considered, false case") {
-    var hospital: Structure =
-      Hospital(
-        infectionProbability,
-        capacity,
-        entranceStrategy = FilteredStrategyFalse(),
-        position = position,
-        treatmentQuality = treatmentQuality
-      )
-    hospital = hospital.tryToEnter(entities.head)
-    hospital.entities.size shouldBe 0
+    var hospitalCopy: Structure = hospital.copy(entranceStrategy = FilteredStrategyFalse())
+    hospitalCopy = hospitalCopy.tryToEnter(entities.head)
+    hospitalCopy.entities.size shouldBe 0
   }
 
   test("In a hospital even if the strategy allow the entity, the capacity must be respected") {
-    var hospital: Structure =
-      Hospital(
-        infectionProbability,
-        capacity,
-        entranceStrategy = FilteredStrategyTrue(),
-        position = position,
-        treatmentQuality = treatmentQuality
-      )
-    hospital = tryToEnterMultiple(hospital, entities)
-    hospital.entities.size shouldBe capacity
+    var hospitalCopy: Structure = hospital.copy(entranceStrategy = FilteredStrategyTrue())
+    hospitalCopy = tryToEnterMultiple(hospitalCopy, entities)
+    hospitalCopy.entities.size shouldBe capacity
   }
 
   private def tryToEnterMultiple(structure: Structure, entities: Seq[String]): Structure =
