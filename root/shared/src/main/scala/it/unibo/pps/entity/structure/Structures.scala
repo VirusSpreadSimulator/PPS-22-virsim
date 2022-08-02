c02e00100bc69d4a498f841a9ef0d7552403bc8d
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-02 16:32:36 +0200

    feat: added hospital to the structures

--- root/shared/src/main/scala/it/unibo/pps/entity/structure/Structures.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/structure/Structures.scala
@@ -6,8 +6,10 @@ import it.unibo.pps.entity.structure.StructureComponent.*
 import it.unibo.pps.entity.structure.entrance.Entrance.EntranceStrategy
 import it.unibo.pps.entity.structure.entrance.Entrance.BaseEntranceStrategy
 import it.unibo.pps.entity.common.GaussianProperty.GaussianDurationTime
+import it.unibo.pps.entity.structure.StructureComponent.Hospitalization.TreatmentQuality
+
 import scala.concurrent.duration.MINUTES
-import monocle.syntax.all._
+import monocle.syntax.all.*
 
 /** This module contains the base structure description and the main implementation that are useful during the
   * simulation configuration
@@ -84,5 +86,39 @@ object Structures:
       with Groupable:
     override type Position = Point2D
     override type Group = String
+    override protected def enter(entity: String): Structure =
+      this.focus(_.entities).modify(_ + entity)
+
+  /** Builder for the GenericBuilding type of structure
+    * @param infectionProbability
+    *   the probability of infection inside the structure
+    * @param capacity
+    *   the capacity of the structure in terms of the number of entities that can enter
+    * @param permanenceTimeDistribution
+    *   gaussian distribution that describe the permanence time
+    * @param entranceStrategy
+    *   the strategy used for discriminate the entities entrance
+    * @param entities
+    *   the entities that are inside the structure.
+    * @param position
+    *   the position of the structure
+    * @param visibilityDistance
+    *   the distance within the structure is visible for an entity
+    * @param treatmentQuality
+    *   the virus treatment quality.
+    */
+  case class Hospital(
+      override val infectionProbability: Double,
+      override val capacity: Int,
+      override val permanenceTimeDistribution: GaussianDurationTime = defaultPermanenceTimeDistribution,
+      override val entranceStrategy: EntranceStrategy = BaseEntranceStrategy(),
+      override val entities: Set[String] = Set(),
+      override val position: Point2D,
+      override val visibilityDistance: Distance = defaultVisibilityDistance,
+      override val treatmentQuality: TreatmentQuality
+  ) extends BaseStructure
+      with Placeable
+      with Hospitalization:
+    override type Position = Point2D
     override protected def enter(entity: String): Structure =
       this.focus(_.entities).modify(_ + entity)
