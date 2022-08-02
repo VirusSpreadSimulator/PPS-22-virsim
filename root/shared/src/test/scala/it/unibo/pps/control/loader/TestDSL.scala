2f5146d3ca72b493673c265f9aad483737de0dd4
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-03 00:43:11 +0200

    test(dsl): add test for virus configuration dsl

--- root/shared/src/test/scala/it/unibo/pps/control/loader/TestDSL.scala
+++ root/shared/src/test/scala/it/unibo/pps/control/loader/TestDSL.scala
@@ -2,20 +2,25 @@ package it.unibo.pps.control.loader
 
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.{Configuration, VirsimConfiguration}
 import it.unibo.pps.control.engine.SimulationComponent.Simulation
+import it.unibo.pps.entity.virus.VirusComponent.Virus
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import it.unibo.pps.control.loader.configuration.ConfigurationDSL.*
+import it.unibo.pps.control.loader.configuration.dsl.SimulationDSL.*
+import it.unibo.pps.control.loader.configuration.dsl.VirusDSL.*
 
 import scala.language.postfixOps
 
 class TestDSL extends AnyFunSuite with Matchers:
 
+  private val simulation: Simulation = Simulation()
+  private val virus: Virus = Virus()
+
   test("Simulation should be configurable via DSL") {
-    Simulation() days 5 entities 200 averagePopulationAge 40 stdDevPopulationAge 0.5 peoplePerHouse 4 startingInfectedPercentage 10
+    simulation days 5 entities 200 averagePopulationAge 40 stdDevPopulationAge 0.5 peoplePerHouse 4 startingInfectedPercentage 10
   }
 
   test("Virus should be configurable via DSL") {
-    ???
+    virus name "Covid-19" spreadRate 1.5 averagePositivityDays 7 severeDeseaseProbability 15
   }
 
   test("Structures should be configurable via DSL") {
@@ -25,7 +30,7 @@ class TestDSL extends AnyFunSuite with Matchers:
   test("Entire environment should be configurable via DSL") {
     VirsimConfiguration(
       Simulation() days 5 entities 200 averagePopulationAge 40 stdDevPopulationAge 0.5 peoplePerHouse 4 startingInfectedPercentage 10,
-      ???,
+      Virus() name "Covid-19" spreadRate 1.5 averagePositivityDays 7 severeDeseaseProbability 15,
       ???
     )
   }
