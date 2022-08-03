0f549e5ab2109a52d233b470e73235be94a36be2
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-03 16:03:51 +0200

    test(time): require that timestamp calculate itself the relative time and the iteration number from absolute values

--- root/shared/src/test/scala/it/unibo/pps/common/TimeTests.scala
+++ root/shared/src/test/scala/it/unibo/pps/common/TimeTests.scala
@@ -2,13 +2,15 @@ package it.unibo.pps.common
 
 import it.unibo.pps.entity.common.Time.DurationTime
 import it.unibo.pps.entity.common.Time.TimeStamp
+import it.unibo.pps.entity.common.Time.TimeConfiguration
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class TimeTests extends AnyFunSuite with Matchers:
+  private val absoluteTime = 1540
   private val time = 100
-  private val iterationNumber = 2
-  private val timeStamp = TimeStamp(time, iterationNumber)
+  private val iterationNumber = 1
+  private val timeStamp = TimeStamp(absoluteTime)
 
   test("It's possible to obtain length and unit from a duration") {
     import scala.concurrent.duration.MILLISECONDS
@@ -17,19 +19,23 @@ class TimeTests extends AnyFunSuite with Matchers:
     duration.unit shouldBe MILLISECONDS
   }
 
-  test("A timestamp tick value should be convertible to its absolute value respect the start of the simulation") {
-    timeStamp.toAbsoluteTickValue() shouldBe (time * iterationNumber)
+  test("Timestamp tick number should be convertible to its relative value respect to the iteration start") {
+    timeStamp.ticksSinceIterationStart shouldBe time
   }
 
   test("A timestamp tick value should be convertible to minutes") {
-    timeStamp.toMinutes() shouldBe time
+    timeStamp.toMinutes() shouldBe (time / TimeConfiguration.TICKS_PER_MINUTE)
   }
 
   test("A timestamp tick value should be convertible to absolute minutes since the start of the simulation") {
-    timeStamp.toAbsoluteMinutes() shouldBe (iterationNumber * timeStamp.toMinutes())
+    timeStamp.toAbsoluteMinutes() shouldBe (absoluteTime / TimeConfiguration.TICKS_PER_MINUTE)
+  }
+
+  test("We can get the current iteration number from a timestamp") {
+    timeStamp.iteration shouldBe iterationNumber
   }
 
   test("Two timestamp should be comparable in order to understand which one is the latest") {
-    val lowerTimestamp = TimeStamp(time, iterationNumber - 1)
+    val lowerTimestamp = TimeStamp(absoluteTime - 1)
     timeStamp > lowerTimestamp shouldBe true
   }
