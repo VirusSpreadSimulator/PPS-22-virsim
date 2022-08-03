795ae59f19697b18d68c9d58e62a6deea184bd98
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-03 17:00:28 +0200

    feat(permanence): added permanence concept

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/structure/entrance/Permanence.scala
@@ -0,0 +1,48 @@
+package it.unibo.pps.entity.structure.entrance
+
+import it.unibo.pps.entity.common.Time.{DurationTime, TimeStamp}
+
+object Permanence:
+  type Entity = String // todo: refactor when entity is defined
+
+  /** Model the permanence status */
+  enum PermanenceStatus:
+    /** VALID: permanence still in its assigned duration time */
+    case VALID
+    /** OVER: permance over its assigned duration time */
+    case OVER
+
+  trait EntityPermanence:
+    /** The entity that is inside the structure
+      * @return
+      *   the entity
+      */
+    def entity: Entity
+    /** The timestamp that represent the entry time of the entity in the structure
+      * @return
+      *   the timestamp
+      */
+    def timestamp: TimeStamp
+    /** The permanence time inside the structure
+      * @return
+      *   the duration time
+      */
+    def permanenceDuration: DurationTime
+    /** Considering the timestamp in witch the entity is entered and the permanence duration time is possibile to
+      * understand if the entity is allowed to stay inside or it need to exit.
+      * @param timestampToCheck
+      *   the timestamp respect witch doing the check.
+      * @return
+      *   a [[PermanceStatus]] that represent the status of the permanence.
+      */
+    def status(timestampToCheck: TimeStamp): PermanenceStatus
+
+  object EntityPermanence:
+    def apply(entity: Entity, timestamp: TimeStamp, permanenceDuration: DurationTime): EntityPermanence =
+      EntityPermanenceImpl(entity, timestamp, permanenceDuration)
+    private case class EntityPermanenceImpl(entity: Entity, timestamp: TimeStamp, permanenceDuration: DurationTime)
+        extends EntityPermanence:
+      override def status(timeStampToCheck: TimeStamp): PermanenceStatus =
+        timeStampToCheck <= timestamp + permanenceDuration match
+          case true => PermanenceStatus.VALID
+          case _ => PermanenceStatus.OVER
