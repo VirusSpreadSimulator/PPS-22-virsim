962e51ce5e5ca6b64046b9b977bbafcd345c104f
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-03 16:04:28 +0200

    feat(time): timestamp calculate itself the relative time and the iteration number from absolute ticks

--- root/shared/src/main/scala/it/unibo/pps/entity/common/Time.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/common/Time.scala
@@ -4,6 +4,12 @@ import scala.concurrent.duration
 import scala.concurrent.duration.{FiniteDuration, TimeUnit}
 
 object Time:
+  object TimeConfiguration:
+    val TICKS_PER_MINUTE = 1
+    val MINUTES_PER_DAY = 1440
+    val DAY_MINUTES_UPPER_BOUND = 1000
+    val TICKS_PER_DAY = MINUTES_PER_DAY * TICKS_PER_MINUTE
+
   /** Alias for finite duration time */
   type DurationTime = FiniteDuration
   /** Factory for [[DurationTime]]
@@ -14,31 +20,31 @@ object Time:
     * @return
     *   [[DurationTime]] instance
     */
-  def DurationTime(length: Int, unit: TimeUnit): DurationTime = FiniteDuration(length, unit)
+  def DurationTime(length: Long, unit: TimeUnit): DurationTime = FiniteDuration(length, unit)
 
   /** Model the TimeStamp concept */
   trait TimeStamp extends Ordered[TimeStamp]:
+    /** Ticks since the start of the simulation.
+      * @return
+      *   the absolute ticks.
+      */
+    def absoluteTicks: Long
     /** Represent the ticks after the current iteration number.
       * @return
       *   the ticks after the current iteration number.
       */
-    def ticksSinceIterationStart: Int
+    def ticksSinceIterationStart: Long
     /** Represent the current iteration number.
       * @return
       *   the iteration number.
       */
-    def iteration: Int
-    /** Method to convert a timestamp to the ticks since the start of the simulation.
-      * @return
-      *   the absolute ticks.
-      */
-    def toAbsoluteTickValue(): Long
+    def iteration: Long
     /** Method to convert the ticks from the engine in the minutes of the virtual time. The minutes returned are respect
       * to the current iteration.
       * @return
       *   the minutes
       */
-    def toMinutes(): Int
+    def toMinutes(): Long
     /** Method to convert the ticks from the engine in the minutes of the virtual time.The minutes returned are respect
       * to the current iteration
       * @return
@@ -47,11 +53,11 @@ object Time:
     def toAbsoluteMinutes(): Long
 
   object TimeStamp:
-    def apply(ticksSinceIterationStart: Int, iteration: Int): TimeStamp =
-      TimeStampImpl(ticksSinceIterationStart, iteration)
-    private case class TimeStampImpl(override val ticksSinceIterationStart: Int, override val iteration: Int)
-        extends TimeStamp:
-      override def toAbsoluteTickValue(): Long = iteration * ticksSinceIterationStart
-      override def toMinutes(): Int = ticksSinceIterationStart
-      override def toAbsoluteMinutes(): Long = iteration * toMinutes()
-      override def compare(that: TimeStamp): Int = (this.toAbsoluteTickValue() - that.toAbsoluteTickValue()).toInt
+    def apply(absoluteTicks: Long): TimeStamp =
+      TimeStampImpl(absoluteTicks)
+    private case class TimeStampImpl(override val absoluteTicks: Long) extends TimeStamp:
+      override def ticksSinceIterationStart: Long = absoluteTicks - iteration * TimeConfiguration.TICKS_PER_DAY
+      override def iteration: Long = absoluteTicks / TimeConfiguration.TICKS_PER_DAY
+      override def toMinutes(): Long = ticksSinceIterationStart / TimeConfiguration.TICKS_PER_MINUTE
+      override def toAbsoluteMinutes(): Long = absoluteTicks / TimeConfiguration.TICKS_PER_MINUTE
+      override def compare(that: TimeStamp): Int = (this.absoluteTicks - that.absoluteTicks).toInt
