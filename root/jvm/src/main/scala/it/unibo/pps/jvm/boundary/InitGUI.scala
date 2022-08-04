36aacdef68fb51be5354830d5bb20238a7187e7f
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-04 17:59:20 +0200

    refactor: renamed utilis

---
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/InitGUI.scala
@@ -0,0 +1,3 @@
+package it.unibo.pps.jvm.boundary class InitGUI {
+
+}
--- root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/Utils.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/Utils.scala
@@ -1,11 +1,16 @@
 package it.unibo.pps.jvm.boundary
 
 import monix.execution.Scheduler
+
+import java.awt.Dimension
 import javax.swing.SwingUtilities
 import scala.concurrent.ExecutionContext
 
 // todo: refactor
-object SwingConfiguration:
+object Utils:
+  given Conversion[(Int, Int), Dimension] with
+    override def apply(x: (Int, Int)): Dimension = Dimension(x._1, x._2)
+
   val swingScheduler: Scheduler = Scheduler.apply(new ExecutionContext {
     override def execute(runnable: Runnable): Unit = SwingUtilities.invokeLater(runnable)
     override def reportFailure(cause: Throwable): Unit = {} // todo: technical debt
