a5f9c5bccb664a9cf593f199531c76b84b9964e5
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-04 16:09:53 +0200

    feat: create the environment of the simulation

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/Environment/Environment.scala
@@ -0,0 +1,26 @@
+package it.unibo.pps.entity.Environment
+
+import it.unibo.pps.entity.common.Space.Point2D
+import it.unibo.pps.entity.common.Time.TimeStamp
+import it.unibo.pps.entity.entity.Entities
+import it.unibo.pps.entity.entity.EntityComponent.Entity
+import it.unibo.pps.entity.structure.StructureComponent.Structure
+import it.unibo.pps.entity.virus.VirusComponent.Virus
+it.unibo.pps.entity.common.Time
+
+/**
+ * The environment represent the container for the properties of the simulation.
+ * @param width the width of the environment.
+ * @param height the height of the environment.
+ * @param simulationDays the number of day of the simulation.
+ * @param entities set that contains all the entity of the simulation.
+ * @param structure set that contains all the entity of the simulation.
+ * @param virus the virus of the simulation.
+ */
+case class Environment(
+  width: Int,
+  height: Int,
+  simulationDays: Long,
+  entities: Set[Entity],
+  structure: Set[Structure],
+  virus: Virus)
