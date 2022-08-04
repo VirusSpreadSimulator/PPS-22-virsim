7949f641569ff9fd4c52e628872bbd68c8f99e82
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-04 15:32:14 +0200

    feat(boundary): added config boundary

--- root/shared/src/main/scala/it/unibo/pps/boundary/BoundaryModule.scala
+++ root/shared/src/main/scala/it/unibo/pps/boundary/BoundaryModule.scala
@@ -3,12 +3,17 @@ import monix.eval.Task
 import monix.reactive.Observable
 import component.Events.Event
 
+import java.nio.file.Path
+
 object BoundaryModule:
   trait Boundary:
     def init(): Task[Unit]
     def start(): Task[Unit]
     def render(i: Int): Task[Unit]
     def events(): Observable[Event]
+  trait ConfigBoundary:
+    def config(): Task[Path]
+    def error(): Task[Unit]
   trait Provider:
     val boundaries: Seq[Boundary]
   trait Interface extends Provider
