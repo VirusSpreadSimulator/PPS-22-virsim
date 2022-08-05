2228af0b077ced0a2001d9d9371e75be747201b9
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-05 11:54:35 +0200

    feat(launcher): add method to manage configuration parsing and interaction with boundary

--- root/shared/src/main/scala/it/unibo/pps/control/launcher/LauncherModule.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/launcher/LauncherModule.scala
@@ -2,21 +2,38 @@ package it.unibo.pps.control.launcher
 
 import it.unibo.pps.boundary.BoundaryModule
 import it.unibo.pps.control.loader.LoaderModule
+import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationResult
+import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationResult.*
 import monix.eval.Task
 
 object LauncherModule:
   trait Launcher:
     def launch(): Task[Unit]
+    def launcherLoop(): Task[Unit]
   trait Provider:
     val launcher: Launcher
   type Requirements = BoundaryModule.Provider with LoaderModule.Provider
   trait Component:
     context: Requirements =>
     class LauncherImpl extends Launcher:
+
       override def launch(): Task[Unit] =
         for
           _ <- Task.sequence(context.boundaries.map(_.init()))
-          _ <- Task(context.loader.load("configuration.scala"))
+          _ <- launcherLoop()
         yield ()
+
+      override def launcherLoop(): Task[Unit] =
+        for
+          configResult <- Task(
+            context.loader.parseConfiguration("configuration.scala")
+          ) //context.boundaries.configBoundary.config
+          _ <- configResult match
+            case ERROR(errors) =>
+              //Task(context.boundaries.configBoundary.error(configResult))
+              launcherLoop()
+            case OK(configuration) => Task(context.loader.startEngine(configuration))
+        yield ()
+
   trait Interface extends Provider with Component:
     self: Requirements =>
