b8d78971027bea4e5ab9d0559678e2d26f958106
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-05 11:47:25 +0200

    feat: add method to initialize the environment with configuration parameters

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/environment/EnvironmentModule.scala
@@ -0,0 +1,38 @@
+package it.unibo.pps.entity.environment
+
+import it.unibo.pps.entity.State
+import it.unibo.pps.entity.entity.EntityComponent.Entity
+import it.unibo.pps.entity.structure.StructureComponent.Structure
+import it.unibo.pps.entity.virus.VirusComponent.Virus
+import it.unibo.pps.control.loader.configuration.dsl.VirusDSL
+import monocle.syntax.all.*
+
+object EnvironmentModule:
+
+  trait Environment:
+    //def time: Time
+    def entities: Set[Entity]
+    def structures: Set[Structure]
+    def virus: Virus
+
+    def initializeEnvironment(entities: Set[Entity], virus: Virus, structures: Set[Structure]): Unit
+
+  trait Provider:
+    val env: Environment
+  trait Component:
+    case class EnvironmentImpl(
+        override val entities: Set[Entity] = Set(),
+        override val structures: Set[Structure] = Set(),
+        override val virus: Virus = Virus()
+    ) extends Environment:
+
+      override def initializeEnvironment(
+          simulationEntities: Set[Entity],
+          simulationVirus: Virus,
+          simulationStructures: Set[Structure]
+      ): Unit =
+        this.focus(_.entities).replace(simulationEntities)
+        this.focus(_.virus).replace(simulationVirus)
+        this.focus(_.structures).replace(simulationStructures)
+
+  trait Interface extends Provider with Component
