ab319679e209e23a9a6f59050b12d63c820a9433
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-05 15:17:01 +0200

    feat: added base simulation gui

--- root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/SimulationGUI.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/SimulationGUI.scala
@@ -5,24 +5,26 @@ import it.unibo.pps.boundary.component.Events.Event
 import it.unibo.pps.jvm.boundary.Utils
 import it.unibo.pps.jvm.boundary.gui.SimulationGUI
 import it.unibo.pps.jvm.boundary.gui.panel.SimulationPanel
+import it.unibo.pps.jvm.boundary.gui.panel.ChartPanel
 import monix.reactive.Observable
 import monix.eval.Task
 
-import java.awt.BorderLayout
-import javax.swing.{BoxLayout, JFrame, JPanel, JScrollPane, ScrollPaneConstants}
+import java.awt.{BorderLayout, GridLayout}
+import javax.swing.{BoxLayout, JFrame, JPanel, JScrollPane, JSplitPane, ScrollPaneConstants, SwingUtilities}
 
 trait SimulationGUI:
   def init(): Task[Unit]
   def render(): Task[Unit]
   def events(): Observable[Event]
 
-object SimulationGUI:
-  def apply(width: Int = 1024, height: Int = 768, title: String = "Virsim"): SimulationGUI =
-    SimulationGUIImpl(width, height, title)
+object SimulationGUI: //todo: group all the magic number in Values
+  def apply(width: Int = 800, height: Int = 700, title: String = "Virsim"): SimulationGUI =
+    SimulationGUIImpl(width + 20, height + 20, title)
   private class SimulationGUIImpl(width: Int, height: Int, title: String) extends SimulationGUI:
     import Utils.given
-    private lazy val simPanel = SimulationPanel()
-    private lazy val chartPanel = JPanel()
+
+    private lazy val simulationPanel = SimulationPanel()
+    private lazy val chartPanel = ChartPanel()
 
     private lazy val container: Task[JFrame] =
       for
@@ -32,36 +34,63 @@ object SimulationGUI:
         _ <- io(frame.setLocationRelativeTo(null))
       yield frame
 
-    private lazy val topPanel: Task[JPanel] =
-      for panel <- io(JPanel())
-      yield panel
+    private lazy val topPanel: Task[JSplitPane] =
+      for
+        _ <- io(simulationPanel.setMinimumSize((500, 500)))
+        _ <- io(chartPanel.setMinimumSize((300, 500)))
+        split <- io(JSplitPane(JSplitPane.HORIZONTAL_SPLIT, simulationPanel, chartPanel))
+        _ <- io(split.setResizeWeight(0.5))
+        _ <- io(split.setOneTouchExpandable(true))
+        _ <- io(split.setContinuousLayout(true))
+      yield split
 
     private lazy val bottomPanel: Task[JPanel] =
-      for panel <- io(JPanel())
+      for
+        panel <- io(JPanel())
+        panelLM <- io(GridLayout(1, 5))
+        _ <- io(panel.setLayout(panelLM))
       yield panel
 
-    private lazy val mainPanel: Task[JPanel] =
-      for mainPanel <- io(JPanel())
-//        _ <- io(mainPanel.add(simPanel, BorderLayout.CENTER))
-      yield mainPanel
+    private lazy val mainPanel: Task[JSplitPane] =
+      for
+        topP <- topPanel
+        _ <- io(topP.setMinimumSize((800, 500)))
+        bottomP <- bottomPanel
+        _ <- io(bottomP.setMinimumSize((800, 200)))
+        split <- io(JSplitPane(JSplitPane.VERTICAL_SPLIT, topP, bottomP))
+        _ <- io(split.setResizeWeight(0.5))
+        _ <- io(split.setOneTouchExpandable(true))
+        _ <- io(split.setContinuousLayout(true))
+      yield split
 
     override def init(): Task[Unit] =
       for
         frame <- container.asyncBoundary(Utils.swingScheduler)
         mainP <- mainPanel
-        _ <- io(frame.setContentPane(mainP))
+        _ <- io(mainP.setOpaque(true))
+        _ <- io(frame.add(mainP))
+        _ <- io(frame.pack())
         _ <- io(frame.setVisible(true))
       yield ()
 
-    override def render() = io(simPanel.display())
+    override def render(): Task[Unit] =
+      for
+        _ <- Task.pure {}.asyncBoundary(Utils.swingScheduler)
+        _ <- io(simulationPanel.updateAndDisplay())
+        _ <- io(chartPanel.updateAndDisplay())
+        _ <- io(println(chartPanel.getSize().width))
+      yield ()
 
     override def events(): Observable[Event] = ???
 
   @main def main(): Unit =
     import monix.execution.Scheduler
+    import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
     given Scheduler = monix.execution.Scheduler.global
+    val gui = SimulationGUI()
+    gui.init().runSyncUnsafe()
     (for
-      gui <- Task(SimulationGUI())
-      _ <- gui.init()
+      _ <- Task.sleep(FiniteDuration(100, MILLISECONDS))
       _ <- gui.render()
-    yield gui).runAsyncAndForget
+      _ <- Task.shift
+    yield gui).loopForever.runAsyncAndForget
