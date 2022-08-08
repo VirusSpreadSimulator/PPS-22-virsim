83c3afba512a6088eb251dd20625fd77019b05cc
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-08 15:38:17 +0200

    chore: added trait that define the type of the panel

--- root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/panel/Panels.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/panel/Panels.scala
@@ -1,6 +1,16 @@
 package it.unibo.pps.jvm.boundary.gui.panel
 
+import it.unibo.pps.boundary.component.Events.Event
+import monix.reactive.Observable
+
 import javax.swing.JPanel
 
-trait UpdateblePanel extends JPanel:
-  def updateAndDisplay(): Unit
+object Panels:
+  trait UpdateblePanel extends JPanel:
+    def updateAndDisplay(): Unit // todo: pass the env
+
+  trait DisplayblePanel extends JPanel:
+    def display(): Unit
+
+  trait EventablePanel:
+    def events: Observable[Event]
