a589a9824ffec56fc2bd95989ff7a5db8f21115e
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-09 10:59:24 +0200

    test: test the mean and std of the gaussian property

--- root/shared/src/test/scala/it/unibo/pps/common/GaussianPropertyTests.scala
+++ root/shared/src/test/scala/it/unibo/pps/common/GaussianPropertyTests.scala
@@ -8,11 +8,19 @@ class GaussianPropertyTests extends AnyFunSuite with Matchers:
   private case class GaussianExample(mean: Double, std: Double) extends Gaussian[Double]:
     override def next(): Double = nextGaussian()
   private val tolerance = 1e-0
+  private val mean = 10
+  private val std = 2
+  private val g = GaussianExample(mean, std)
+
+  test("A gaussian property must allow to obtain the mean") {
+    g.mean shouldBe mean
+  }
+
+  test("A gaussian property must allow to obtain the standard deviation") {
+    g.std shouldBe std
+  }
 
   test("A gaussian property must respect the mean for a thousands of elements with a tolerance of +-1") {
-    val mean = 10
-    val std = 2
-    val g = GaussianExample(mean, std)
     val elems: Seq[Double] = for
       _ <- 0 to 1000
       next = g.next()
