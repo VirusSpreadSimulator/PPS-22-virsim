13bbb3553eaa83ca1d0953a3ac1e6e03c4c4270e
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-09 11:21:36 +0200

    feat: added switch mask obligation event

--- root/shared/src/main/scala/it/unibo/pps/boundary/component/Events.scala
+++ root/shared/src/main/scala/it/unibo/pps/boundary/component/Events.scala
@@ -6,6 +6,10 @@ object Events:
     case Hit(number: Int) // todo: delete
     /** The user signal the intent to pause the simulation. */
     case Pause
+    /** The user signal the intent to stop the simulation */
+    case Stop
+    /** The user signal the intent to switch the obligation of the mask */
+    case SwitchMaskObligation
     /** The user signal the intent to open/close a structure
       * @param group:
       *   the group to open/close
