216c4d2f78d53c5d6ef5c5485ffc298177e301a8
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-10 16:06:58 +0200

    chore: add parser as given instance

--- root/shared/src/main/scala/it/unibo/pps/control/loader/LoaderModule.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/loader/LoaderModule.scala
@@ -6,7 +6,6 @@ import it.unibo.pps.control.loader.configuration.ConfigurationComponent.{
   ConfigurationResult,
   VirsimConfiguration
 }
-import it.unibo.pps.control.loader.configuration.ConfigurationComponent.given
 import it.unibo.pps.control.loader.configuration.dsl.SimulationDSL.simulation
 import it.unibo.pps.control.loader.configuration.dsl.VirusDSL.virus
 import it.unibo.pps.control.loader.configuration.dsl.StructuresDSL.structures
@@ -20,12 +19,14 @@ import it.unibo.pps.entity.entity.Entities.*
 import it.unibo.pps.entity.entity.EntityComponent.Moving.MovementGoal
 import it.unibo.pps.entity.common.GaussianProperty.GaussianIntDistribution
 import it.unibo.pps.entity.common.Space.Point2D
+import it.unibo.pps.control.loader.configuration.ConfigurationComponent.given
 import it.unibo.pps.entity.structure.Structures.House
 import it.unibo.pps.control.loader.configuration.SimulationDefaults.GlobalDefaults
 import it.unibo.pps.control.loader.configuration.SimulationDefaults.MAX_VALUES
 import it.unibo.pps.control.loader.configuration.SimulationDefaults.MIN_VALUES
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationError
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationError.*
+import it.unibo.pps.control.loader.configuration.ConfigurationParser
 import monocle.syntax.all.*
 import monix.eval.Task
 
@@ -35,13 +36,12 @@ import scala.io.Source
 object LoaderModule:
 
   trait Loader:
-
     /** @param configurationFile
       *   the configuration file with simulation parameters.
       * @return
       *   the result of the configuration parsing.
       */
-    def parseConfiguration(configurationFile: String): Task[ConfigurationResult]
+    def parseConfiguration(configurationFile: String)(using parser: ConfigurationParser): Task[ConfigurationResult]
 
     /** @param configuration
       *   The configuration of the simulation, structures and virus.
@@ -65,28 +65,20 @@ object LoaderModule:
 
     class LoaderImpl extends Loader:
 
-      private def readFile(path: String): Task[String] =
-        Task(GlobalDefaults.DSL_IMPORTS + Source.fromFile(path).getLines().mkString)
-
-      private def reflectConfiguration(program: String)(using engine: ScriptEngine): Task[Option[Configuration]] =
-        val canBeReflected = engine.eval(program).isInstanceOf[VirsimConfiguration]
-        if canBeReflected then Task(Some(engine.eval(program).asInstanceOf[VirsimConfiguration])) else Task(None)
-
-      private def checkErrors(configuration: Configuration): Task[ConfigurationResult] =
-        Task(ConfigurationResult.OK(configuration))
-
-      override def parseConfiguration(configurationFile: String): Task[ConfigurationResult] =
+      override def parseConfiguration(configurationFile: String)(using
+          parser: ConfigurationParser
+      ): Task[ConfigurationResult] =
         for
-          program <- readFile(configurationFile)
-          configuration <- reflectConfiguration(program)
+          program <- parser.readFile(configurationFile)
+          configuration <- parser.reflectConfiguration(program)
           parsingResult <- configuration match
             case None =>
               Task(
                 ConfigurationResult.ERROR(
-                  ConfigurationError.INVALID_FILE("Invalid Scala file! Please check our DSL documentation !")
+                  List(ConfigurationError.INVALID_FILE("Invalid Scala file! Please check our DSL documentation !"))
                 )
               )
-            case Some(configuration: Configuration) => checkErrors(configuration)
+            case Some(configuration: Configuration) => parser.checkErrors(configuration)
         yield parsingResult
 
       override def createEnvironment(configuration: Configuration): Environment =
