fa15aec36547aed533ff2e0450b83cfca7322edb
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-11 12:49:15 +0200

    refactor: refactor simulation environment draw through Drawable type class

--- root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/panel/SimulationPanel.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/panel/SimulationPanel.scala
@@ -2,14 +2,9 @@ package it.unibo.pps.jvm.boundary.gui.panel
 
 import it.unibo.pps.jvm.boundary.gui.panel.Panels.UpdateblePanel
 
-import java.awt.geom.AffineTransform
-import javax.swing.JPanel
-import java.awt.{Color, Dimension, Font, Graphics, Graphics2D, RenderingHints}
+import java.awt.{Graphics, Graphics2D, RenderingHints}
 import it.unibo.pps.boundary.ViewUtils.io
-import it.unibo.pps.entity.entity.Entities.BaseEntity
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
-import it.unibo.pps.entity.structure.StructureComponent.Visible
-import it.unibo.pps.entity.structure.Structures.{GenericBuilding, Hospital, House, SimulationStructure}
 import it.unibo.pps.jvm.boundary.Values.SimulationColor
 import monix.eval.Task
 
@@ -31,68 +26,8 @@ class SimulationPanel() extends UpdateblePanel:
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
       g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
 
-      drawStructures(g2, environment, scale)
-      drawEntities(g2, environment, scale)
-
-  private def drawStructures(g: Graphics2D, environment: Environment, scale: Int): Unit =
-    for struct <- environment.structures do drawStructure(g, struct, scale)
-
-  private def drawStructure(g: Graphics2D, structure: SimulationStructure, scale: Int): Unit =
-    structure match
-      case house: House =>
-        g.setColor(SimulationColor.HOUSE_COLOR)
-      case generic: GenericBuilding =>
-        drawStructureVisibility(g, generic, scale)
-        g.setColor(if generic.isOpen then SimulationColor.GENERIC_COLOR_OPEN else SimulationColor.GENERIC_COLOR_CLOSED)
-      case hospital: Hospital =>
-        drawStructureVisibility(g, hospital, scale)
-        g.setColor(SimulationColor.HOSPITAL_COLOR)
-    g.fillRect(scaleToView(structure.position.x, scale), scaleToView(structure.position.y, scale), scale, scale)
-    drawRemainedCapacity(g, structure, scale)
-
-  private def drawRemainedCapacity(g: Graphics2D, structure: SimulationStructure, scale: Int): Unit =
-    val message = s"${structure.capacity - structure.entities.size}"
-    val font = Font(Font.SANS_SERIF, Font.PLAIN, 10)
-    val scaleFont = AffineTransform()
-    val fontMetrics = g.getFontMetrics(font)
-    val scaleF = Math.min(
-      scale.toDouble / (fontMetrics.getMaxAscent + fontMetrics.getMaxDescent),
-      scale.toDouble / fontMetrics.stringWidth(message)
-    )
-    scaleFont.scale(scaleF, scaleF)
-    g.setFont(font.deriveFont(scaleFont))
-    g.setColor(Color.WHITE)
-    g.drawString(
-      message,
-      scaleToView(structure.position.x, scale),
-      scaleToView(structure.position.y + 1, scale)
-    )
-
-  private def drawStructureVisibility(g: Graphics2D, structure: SimulationStructure with Visible, scale: Int): Unit =
-    g.setColor(SimulationColor.VISIBILITY_RANGE_COLOR)
-    g.fillOval(
-      scaleToView(structure.position.x - structure.visibilityDistance.toInt, scale),
-      scaleToView(structure.position.y - structure.visibilityDistance.toInt, scale),
-      scaleToView(structure.visibilityDistance.toInt * 2 + 1, scale),
-      scaleToView(structure.visibilityDistance.toInt * 2 + 1, scale)
-    )
-
-  private def drawEntities(g: Graphics2D, environment: Environment, scale: Int): Unit =
-    for entity <- environment.entities do
-      entity match
-        case entity: BaseEntity =>
-          g.setColor(
-            SimulationColor.ageColor(
-              if entity.infection.isDefined then SimulationColor.INFECTED_ENTITY_COLOR
-              else SimulationColor.HEALTHY_ENTITY_COLOR,
-              entity.age
-            )
-          )
-          g.fillOval(scaleToView(entity.position.x, scale), scaleToView(entity.position.y, scale), scale, scale)
-          g.setColor(if entity.immunity > 0 then SimulationColor.IMMUNITY_COLOR else Color.WHITE)
-          g.drawOval(scaleToView(entity.position.x, scale), scaleToView(entity.position.y, scale), scale, scale)
-
-  private def scaleToView(coordinate: Int, scale: Int): Int = coordinate * scale
+      import it.unibo.pps.jvm.boundary.component.Drawables.given
+      environment.draw(g2, scale)
 
   override def init(): Task[Unit] = io(setOpaque(true))
 
