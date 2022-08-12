f10177688c89ec2890b77a35d29155d8fb1f646b
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-12 14:40:43 +0200

    refactor: renamed environment status

--- root/shared/src/main/scala/it/unibo/pps/entity/environment/EnvironmentStatus.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/environment/EnvironmentStatus.scala
@@ -5,6 +5,6 @@ enum EnvironmentStatus:
   /** [[Evolving]] means that is currently under update from the engine */
   case EVOLVING
   /** [[Pause]] means that the environment is in a paused state, no event logics, no updates will be computed on it */
-  case PAUSE
+  case PAUSED
   /** [[Stop]] means that the environment is no more evolving in a definitive way */
-  case STOP
+  case STOPPED
