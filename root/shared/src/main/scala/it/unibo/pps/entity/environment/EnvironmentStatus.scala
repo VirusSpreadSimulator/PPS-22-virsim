b75f99cbf3e94e7edb23cc6db36115d608637fbd
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-12 12:22:29 +0200

    chore: added environment status

---
+++ root/shared/src/main/scala/it/unibo/pps/entity/environment/EnvironmentStatus.scala
@@ -0,0 +1,10 @@
+package it.unibo.pps.entity.environment
+
+/** It represent the current status of the environment */
+enum EnvironmentStatus:
+  /** [[Evolving]] means that is currently under update from the engine */
+  case EVOLVING
+  /** [[Pause]] means that the environment is in a paused state, no event logics, no updates will be computed on it */
+  case PAUSE
+  /** [[Stop]] means that the environment is no more evolving in a definitive way */
+  case STOP
