c3d377c2cd583814a6bc8cc0d7f3b4c8f3aca5b9
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-16 17:25:13 +0200

    chore: add scala parser in jvm launcher

--- root/jvm/src/main/scala/it/unibo/pps/jvm/Launcher.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/Launcher.scala
@@ -3,12 +3,16 @@ package it.unibo.pps.jvm
 import it.unibo.pps.boundary.BoundaryModule
 import it.unibo.pps.boundary.BoundaryModule.ConfigBoundary
 import it.unibo.pps.control.launcher.Launch
+import it.unibo.pps.control.parser.ParserModule
 import it.unibo.pps.jvm.boundary.GUIModule
+import it.unibo.pps.jvm.parser.ScalaParser
 
-object Launcher extends Launch with GUIModule.Interface:
+object Launcher extends Launch with GUIModule.Interface with ScalaParser.Interface:
 
   override val gui = GUIBoundaryImpl()
   override val configBoundary: ConfigBoundary = gui
   override val boundaries = Seq(gui)
+  override val scalaParser: ParserModule.Parser = ParserImpl()
+  override val parser: ParserModule.Parser = scalaParser
 
   @main def main(): Unit = launch()
