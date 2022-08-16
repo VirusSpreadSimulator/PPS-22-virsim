0dd0a0fbe21e2902d04b540191aa47ebeb3250b1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-16 17:25:25 +0200

    chore: add yaml parser in js launcher

--- root/js/src/main/scala/it/unibo/pps/js/Launcher.scala
+++ root/js/src/main/scala/it/unibo/pps/js/Launcher.scala
@@ -2,11 +2,15 @@ package it.unibo.pps.js
 
 import it.unibo.pps.boundary.BoundaryModule
 import it.unibo.pps.control.launcher.Launch
+import it.unibo.pps.control.parser.ParserModule
 import it.unibo.pps.js.boundary.JSGUIModule
+import it.unibo.pps.js.parser.YAMLParser
 
-object Launcher extends Launch with JSGUIModule.Interface:
+object Launcher extends Launch with JSGUIModule.Interface with YAMLParser.Interface:
   override val jsGui = JSGUIBoundaryImpl()
   override val configBoundary = jsGui
   override val boundaries = Seq(jsGui)
+  override val YAMLParser: ParserModule.Parser = ParserImpl()
+  override val parser: ParserModule.Parser = YAMLParser
 
   @main def main(): Unit = launch()
