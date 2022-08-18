15d87650a076b56c9279468affa1e2306cdfe445
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-18 14:21:58 +0200

    chore: now immunity type is a double

--- root/shared/src/main/scala/it/unibo/pps/control/engine/logics/EntityLogic.scala
+++
@@ -1,6 +0,0 @@
-package it.unibo.pps.control.engine.logics
-
-import it.unibo.pps.entity.environment.EnvironmentModule.Environment
-
-trait EntityLogic:
-  def execute(environment: Environment): Environment
