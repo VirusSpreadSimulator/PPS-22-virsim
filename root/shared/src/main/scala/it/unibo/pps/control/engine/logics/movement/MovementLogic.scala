1e916159a0ed61a702c4eef105d54d3854fc5103
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-18 16:03:47 +0200

    chore: now the movement logic return a task

--- root/shared/src/main/scala/it/unibo/pps/control/engine/logics/movement/MovementLogic.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/engine/logics/movement/MovementLogic.scala
@@ -1,29 +1,46 @@
 package it.unibo.pps.control.engine.logics.movement
 
-import it.unibo.pps.control.engine.logics.EntityLogic
+import it.unibo.pps.control.engine.logics.Logic.UpdateLogic
 import it.unibo.pps.entity.common.Space.Point2D
+import it.unibo.pps.entity.common.Utils.*
 import it.unibo.pps.entity.entity.Entities.{BaseEntity, SimulationEntity}
+import it.unibo.pps.entity.entity.EntityComponent.Moving
+import it.unibo.pps.entity.entity.EntityComponent.Infectious
 import it.unibo.pps.entity.entity.EntityComponent.Moving.MovementGoal
+import it.unibo.pps.entity.entity.Infection
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
 import it.unibo.pps.prolog.PrologNextMovement
+import monix.eval.Task
 
-class MovementLogic extends EntityLogic:
+class MovementLogic extends UpdateLogic:
+  type MovingEntity = SimulationEntity with Moving with Infectious
 
-  override def execute(environment: Environment): Environment =
-    val updatedEntities: Set[SimulationEntity] =
-      for
-        entity <- environment.externalEntities
-        id = entity.id
-        age = entity.age
-        home = entity.home
-        position = calculateNewPosition(entity.asInstanceOf[BaseEntity], environment.gridSide)
-        infection = entity.asInstanceOf[BaseEntity].infection
-        updatedEntity: SimulationEntity = BaseEntity(id, age, home, position = position, infection = infection)
-      yield updatedEntity
-
-    environment.update(
-      externalEntities = updatedEntities
-    )
+  override def apply(environment: Environment): Task[Environment] =
+//    val updatedEntities: Set[SimulationEntity] =
+//      for
+//        entity <- environment.externalEntities
+//        id = entity.id
+//        age = entity.age
+//        home = entity.home
+//        position = calculateNewPosition(entity.asInstanceOf[BaseEntity], environment.gridSide)
+//        infection = entity.asInstanceOf[BaseEntity].infection
+//        updatedEntity: SimulationEntity = BaseEntity(id, age, home, position = position, infection = infection)
+//      yield updatedEntity
+//
+    for
+      entities <- Task(environment.externalEntities.select[MovingEntity])
+      moved <- Task {
+        entities.map(e =>
+          BaseEntity(
+            e.id,
+            e.age,
+            e.home,
+            position = calculateNewPosition(e, environment.gridSide),
+            infection = e.infection
+          )
+        )
+      }
+    yield environment.update(externalEntities = moved.select[SimulationEntity])
 
   /** Extracts random position given the set of possible position
     * @param set
@@ -43,7 +60,7 @@ class MovementLogic extends EntityLogic:
     * @return
     *   the updated position of the entity
     */
-  private def calculateNewPosition(entity: BaseEntity, gridSide: Int): Point2D =
+  private def calculateNewPosition(entity: MovingEntity, gridSide: Int): Point2D =
     entity.movementGoal match
       case MovementGoal.RANDOM_MOVEMENT =>
         extractRandomPosition(
