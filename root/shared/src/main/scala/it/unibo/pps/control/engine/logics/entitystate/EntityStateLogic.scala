48c5596295a9d8edd395df53698b7681be1e93c9
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-19 11:27:23 +0200

    feat: update entity state logic defined

--- root/shared/src/main/scala/it/unibo/pps/control/engine/logics/entitystate/EntityStateLogic.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/engine/logics/entitystate/EntityStateLogic.scala
@@ -1,29 +1,73 @@
 package it.unibo.pps.control.engine.logics.entitystate
 
 import it.unibo.pps.control.engine.logics.Logic.UpdateLogic
+
 import it.unibo.pps.entity.entity.Entities.SimulationEntity
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
 import it.unibo.pps.entity.common.Utils.*
 import it.unibo.pps.entity.entity.EntityComponent.{Infectious, Moving}
 import monix.eval.Task
-import monocle.syntax.all._
+import it.unibo.pps.control.loader.configuration.SimulationDefaults.*
+import it.unibo.pps.entity.entity.Infection.Severity
+import monocle.syntax.all.*
 
 object EntityStateLogic:
+  /** Logic to update the state of all the entities inside the environment */
   class UpdateEntityStateLogic extends UpdateLogic:
-    override def apply(env: Environment): Task[Environment] = ???
+    import it.unibo.pps.control.engine.logics.entitystate.EntityStateLogic.Updates.*
+    override def apply(env: Environment): Task[Environment] =
+      for
+        environmentExternalUpdated <- updateExternalEntitiesHealth(env)
+        environmentExtIntUpdated <- updateInternalEntitiesHealth(environmentExternalUpdated)
+      yield environmentExtIntUpdated
 
   private object Updates:
-    ???
-/** Updates on entities health
-  * @param entities
-  *   the entities to update
-  * @return
-  *   a task of the set of entity that survived
-  */
-//    def updateEntitiesHealth(entities: Set[SimulationEntity]): Task[Set[SimulationEntity]] =
-//      for
-//        originalEntities <- Task(entities.select[BaseEntity])
-//        updatedInfected <- Task {
-//          originalEntities.filter(_.infection.isDefined).map(entity => entity.focus(_.h))
-//        }
-//      yield ()
+    def updateInternalEntitiesHealth(env: Environment): Task[Environment] =
+      for
+        structures <- Task(env.structures)
+        updatedStructures <- Task {
+          for struct <- structures
+          yield struct.updateEntitiesInside(entity =>
+            Some(handleSingleEntity(entity, env)).filter(_.health > MIN_VALUES.MIN_HEALTH)
+          )
+        }
+        deadEntities <- Task {
+          structures
+            .flatMap(_.entities)
+            .map(_.entity)
+            .filter(entity => !updatedStructures.flatMap(_.entities).map(_.entity.id).contains(entity.id))
+        }
+      yield env.update(structures = updatedStructures, deadEntities = env.deadEntities ++ deadEntities)
+
+    def updateExternalEntitiesHealth(env: Environment): Task[Environment] =
+      for
+        entities <- Task(env.externalEntities)
+        updatedEntities <- Task(entities.map(handleSingleEntity(_, env)))
+        deadEntities <- Task(updatedEntities.filter(_.health <= MIN_VALUES.MIN_HEALTH))
+      yield env.update(
+        externalEntities = updatedEntities -- deadEntities,
+        deadEntities = env.deadEntities ++ deadEntities
+      )
+
+    def handleSingleEntity(entity: SimulationEntity, env: Environment): SimulationEntity = entity.infection match
+      case Some(infection) =>
+        entity
+          .focus(_.health)
+          .modify(_ - VirusDefaults.HEALTH_INFECTED_LOSS * getSeverityValue(infection.severity))
+          .focus(_.infection)
+          .modify(infection => infection.filter(inf => inf.timeOfTheInfection + inf.duration > env.time))
+          .andIf(_.infection.isEmpty)(
+            _.focus(_.immunity).modify(i => Math.min(MAX_VALUES.MAX_IMMUNITY, i + VirusDefaults.IMMUNITY_GAIN_RECOVERY))
+          )
+      case None =>
+        entity
+          .focus(_.health)
+          .modify(value => Math.min(value + VirusDefaults.HEALTH_GAIN, entity.maxHealth))
+          .focus(_.immunity)
+          .modify(i => Math.max(MIN_VALUES.MIN_IMMUNITY, i - VirusDefaults.IMMUNITY_LOSS))
+
+    // todo: to be deleted after Severity modification
+    def getSeverityValue(s: Severity): Double = s match {
+      case Severity.SERIOUS(value) => value;
+      case Severity.LIGHT(value) => value
+    }
---
+++ root/shared/src/test/scala/it/unibo/pps/control/engine/logics/infection/EntityStateLogicTest.scala
@@ -0,0 +1,137 @@
+package it.unibo.pps.control.engine.logics.infection
+
+import it.unibo.pps.control.engine.logics.Logic.UpdateLogic
+import it.unibo.pps.entity.environment.EnvironmentModule.Environment
+import it.unibo.pps.control.engine.logics.entitystate.EntityStateLogic.UpdateEntityStateLogic
+import it.unibo.pps.control.loader.configuration.SimulationDefaults.MIN_VALUES
+import it.unibo.pps.entity.Samples
+import it.unibo.pps.entity.entity.Entities.SimulationEntity
+import weaver.monixcompat.SimpleTaskSuite
+
+object EntityStateLogicTest extends SimpleTaskSuite:
+  val baseEnv: Environment = Samples.sampleEnv
+  val entityStateLogic: UpdateLogic = UpdateEntityStateLogic()
+
+  test("External healthy entities health must be increased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = baseEnv.externalEntities.filter(
+        _.infection.isEmpty
+      ) // Necessary because entities can recovery from the infection and then they would partecipate in the computation
+    yield expect(
+      entitiesToConsider.totalHealth < updatedEnv.externalEntities
+        .filter(_.infection.isEmpty)
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .totalHealth
+    )
+  }
+
+  test("Internal to structure healthy entities health must be increased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = baseEnv.internalEntities.filter(_.infection.isEmpty)
+    yield expect(
+      entitiesToConsider.totalHealth < updatedEnv.internalEntities
+        .filter(_.infection.isEmpty)
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .totalHealth
+    )
+  }
+
+  test("External infected entities health must be decreased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = updatedEnv.externalEntities
+        .filter(_.infection.isDefined)
+    yield expect(
+      baseEnv.externalEntities
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .filter(_.infection.isDefined)
+        .totalHealth > entitiesToConsider.totalHealth
+    )
+  }
+
+  test("Internal to structure infected entities health must be decreased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = updatedEnv.internalEntities.filter(_.infection.isDefined)
+    yield expect(
+      baseEnv.internalEntities
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .filter(_.infection.isDefined)
+        .totalHealth >
+        entitiesToConsider.totalHealth
+    )
+  }
+
+  test("Entities health must be within their max health") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(updatedEnv.allEntities.forall(e => e.health <= e.maxHealth))
+  }
+
+  test("Alive entities must have health greater than min health") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(updatedEnv.allEntities.forall(e => e.health > MIN_VALUES.MIN_HEALTH))
+  }
+
+  test("Dead entities cardinality must be greater or equal than before") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(updatedEnv.deadEntities.size >= baseEnv.deadEntities.size)
+  }
+
+  test("External healthy entities immunity must be decreased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = baseEnv.externalEntities.filter(_.infection.isEmpty)
+    yield expect(
+      entitiesToConsider.totalImmunity > updatedEnv.externalEntities
+        .filter(_.infection.isEmpty)
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .totalImmunity
+    )
+  }
+
+  test("Internal to structure healthy entities immunity must be decreased") {
+    for
+      updatedEnv <- entityStateLogic(baseEnv)
+      entitiesToConsider = baseEnv.internalEntities.filter(_.infection.isEmpty)
+    yield expect(
+      entitiesToConsider.totalImmunity > updatedEnv.internalEntities
+        .filter(_.infection.isEmpty)
+        .filter(e => entitiesToConsider.map(_.id).contains(e.id))
+        .totalImmunity
+    )
+  }
+
+  test("It must not exist an infection that is over in external entities") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(
+      updatedEnv.externalEntities
+        .filter(_.infection.isDefined)
+        .map(_.infection.get)
+        .forall(i => i.timeOfTheInfection + i.duration > updatedEnv.time)
+    )
+  }
+
+  test("It must not exist an infection that is over in internal to structures entities") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(
+      updatedEnv.internalEntities
+        .filter(_.infection.isDefined)
+        .map(_.infection.get)
+        .forall(i => i.timeOfTheInfection + i.duration > updatedEnv.time)
+    )
+  }
+
+  test("The sum of all alive and dead entities remain the same") {
+    for updatedEnv <- entityStateLogic(baseEnv)
+    yield expect(
+      baseEnv.allEntities.size + baseEnv.deadEntities.size == updatedEnv.allEntities.size + updatedEnv.deadEntities.size
+    )
+  }
+
+extension (entities: Set[SimulationEntity])
+  def totalHealth: Double = entities.toSeq.map(_.health).sum
+  def totalImmunity: Double = entities.toSeq.map(_.immunity).sum
+extension (env: Environment)
+  def internalEntities: Set[SimulationEntity] = env.structures.flatMap(_.entities).map(_.entity)
