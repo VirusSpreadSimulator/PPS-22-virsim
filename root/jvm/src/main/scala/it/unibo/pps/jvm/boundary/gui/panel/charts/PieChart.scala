bcd1a55586577d5446162d12380df0f85faefdfe
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-19 16:51:18 +0200

    feat(chart): create pie chart for hospital pressure

---
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/boundary/gui/panel/charts/PieChart.scala
@@ -0,0 +1,47 @@
+package it.unibo.pps.jvm.boundary.gui.panel.charts
+
+import it.unibo.pps.boundary.ViewUtils.io
+import it.unibo.pps.boundary.exporter.Extractors.{DataExtractor, Hospitalized, HospitalFreeSeats}
+import it.unibo.pps.boundary.exporter.StatisticalData.Stats
+import it.unibo.pps.boundary.exporter.StatisticalData.given
+import it.unibo.pps.entity.environment.EnvironmentModule.Environment
+import it.unibo.pps.jvm.boundary.Values.SimulationColor
+import it.unibo.pps.jvm.boundary.gui.panel.charts.Charts.{Chart, MyChartPanel}
+import monix.eval.Task
+import org.jfree.chart.labels.BubbleXYItemLabelGenerator
+import org.jfree.chart.plot.PiePlot
+import org.jfree.chart.{ChartFactory, ChartPanel, JFreeChart}
+import org.jfree.data.general.DefaultPieDataset
+
+import java.awt.{Color, Font}
+import scala.reflect.ClassTag.Nothing
+
+class PieChart extends Chart:
+
+  private val dataset = new DefaultPieDataset[String]()
+  private val pieChart: JFreeChart =
+    ChartFactory.createPieChart("Hospitals Pressure", dataset, true, true, false)
+  private val pieChartPanel: ChartPanel = new MyChartPanel(pieChart)
+  private val extractors: List[DataExtractor[Int]] = List(Hospitalized(), HospitalFreeSeats())
+
+  override def init(): Task[Unit] =
+    for
+      _ <- io(pieChart.getTitle.setFont(new Font("Times New Roman", Font.BOLD, 16)))
+      _ <- io(pieChart.getPlot.asInstanceOf[PiePlot[String]].setSimpleLabels(true))
+      _ <- io(pieChart.getPlot.asInstanceOf[PiePlot[String]].setSectionPaint(extractors.head.name, Color.RED))
+      _ <- io(pieChart.getPlot.asInstanceOf[PiePlot[String]].setSectionPaint(extractors(1).name, Color.YELLOW))
+      _ <- io(pieChart.getPlot.setBackgroundPaint(Color.WHITE))
+      _ <- io(pieChart.setBackgroundPaint(SimulationColor.BACKGROUND_CHART_PANEL_COLOR))
+      _ <- io(pieChartPanel.setBackground(SimulationColor.BACKGROUND_CHART_PANEL_COLOR))
+      _ <- io(pieChartPanel.setVisible(true))
+    yield ()
+
+  override def update(env: Environment): Task[Unit] =
+    for
+      _ <- io {
+        for extractor <- extractors do dataset.setValue(extractor.name, extractor.extractData(env))
+      }
+    yield ()
+
+  override def chart: JFreeChart = pieChart
+  override def chartPanel: ChartPanel = pieChartPanel
