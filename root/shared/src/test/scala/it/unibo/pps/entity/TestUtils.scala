5ed9c0562f097039ac252eb49201cba8cc8b1f29
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-20 15:57:20 +0200

    chore: added functionalities

--- root/shared/src/test/scala/it/unibo/pps/entity/TestUtils.scala
+++ root/shared/src/test/scala/it/unibo/pps/entity/TestUtils.scala
@@ -2,7 +2,16 @@ package it.unibo.pps.entity
 
 import it.unibo.pps.entity.entity.Entities.SimulationEntity
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
+import it.unibo.pps.entity.structure.StructureComponent.Hospitalization
+import it.unibo.pps.entity.structure.Structures.SimulationStructure
+import it.unibo.pps.entity.common.Utils.*
 
 object TestUtils:
   extension (env: Environment)
     def internalEntities: Set[SimulationEntity] = env.structures.flatMap(_.entities).map(_.entity)
+    def hospitals: Set[SimulationStructure with Hospitalization] =
+      env.structures.select[SimulationStructure with Hospitalization]
+
+  extension (entities: Set[SimulationEntity])
+    def totalHealth: Double = entities.toSeq.map(_.health).sum
+    def totalImmunity: Double = entities.toSeq.map(_.immunity).sum
