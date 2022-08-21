db2401fcc6b44949c240e53058fff928cc6effa5
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-22 00:08:03 +0200

    chore: try to solve compatibility jvm-js graphic issue in drawable

--- root/shared/src/main/scala/it/unibo/pps/boundary/component/drawable/Drawable.scala
+++ root/shared/src/main/scala/it/unibo/pps/boundary/component/drawable/Drawable.scala
@@ -2,26 +2,16 @@ package it.unibo.pps.boundary.component.drawable
 
 import java.awt.Graphics2D
 
-/** Type class for extending a type with draw capabilities
+/** Base type class for extending a type with draw capabilities
   * @tparam A
   *   the type to extend
   */
 trait Drawable[A]:
+  type Graphic
   /** Method to draw the element
     * @param g:
     *   the graphics needed to draw
     * @param scale:
     *   the scale factor to apply
     */
-  extension (elem: A) def draw(g: Graphics2D, scale: Int): Unit
-
-/** A set of operations modeled on the Drawable type-class */
-object DrawableOps:
-  /** Method to draw all the elements in a set
-    * @param g
-    *   : the graphics needed to draw
-    * @param scale
-    *   : the scale factor to apply
-    */
-  extension [A: Drawable](drawables: Set[A])
-    def drawAll(g: Graphics2D, scale: Int): Unit = for drawable <- drawables do drawable.draw(g, scale)
+  extension (elem: A) def draw(g: Graphic, scale: Int): Unit
