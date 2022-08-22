b37fecb81ef1ed82d017dbc9ebee2d0a0ddf88e8
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-22 17:20:22 +0200

    chore: add Point class for structure position

--- root/shared/src/main/scala/it/unibo/pps/control/loader/configuration/dsl/StructuresDSL.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/loader/configuration/dsl/StructuresDSL.scala
@@ -1,5 +1,7 @@
 package it.unibo.pps.control.loader.configuration.dsl
 
+import it.unibo.pps.control.loader.configuration.SimulationDefaults.GlobalDefaults
+import it.unibo.pps.entity.common.Space.Point2D
 import it.unibo.pps.entity.structure.Structures.SimulationStructure
 
 object StructuresDSL:
@@ -9,3 +11,5 @@ object StructuresDSL:
   extension [A](set: Set[A])
     def are(variable: A): Set[A] = set + variable
     def and(variable: A): Set[A] = set + variable
+
+  class Point(x: Int, y: Int) extends Point2D(x * GlobalDefaults.GRID_MULTIPLIER, y * GlobalDefaults.GRID_MULTIPLIER)
