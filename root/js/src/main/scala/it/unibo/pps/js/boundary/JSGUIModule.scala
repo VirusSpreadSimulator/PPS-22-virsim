df020306f30ea5fcb18e62aa646be625c7f90833
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-23 14:30:48 +0200

    feat(js): implemented first version of the js gui

--- root/js/src/main/scala/it/unibo/pps/js/boundary/JSGUIModule.scala
+++ root/js/src/main/scala/it/unibo/pps/js/boundary/JSGUIModule.scala
@@ -5,6 +5,7 @@ import it.unibo.pps.boundary.component.Events.Event
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationError
 import it.unibo.pps.control.parser.ReaderModule.FilePath
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
+import it.unibo.pps.js.boundary.gui.JSGUI
 import monix.eval.Task
 import monix.reactive.Observable
 
@@ -14,11 +15,11 @@ object JSGUIModule:
   trait Component:
     class JSGUIBoundaryImpl extends ConfigBoundary:
       private val guiJs = JSGUI()
-      override def init() = guiJs.init()
+      override def init(): Task[Unit] = guiJs.init()
       override def config(): Task[FilePath] = guiJs.config()
       override def error(errors: Seq[ConfigurationError]): Task[Unit] = guiJs.error(errors)
-      override def start() = Task.pure {}
-      override def stop() = Task.pure {}
-      override def consume(env: Environment) = guiJs.render(0)
+      override def start(): Task[Unit] = guiJs.start()
+      override def stop(): Task[Unit] = guiJs.stop()
+      override def consume(env: Environment): Task[Unit] = guiJs.render(env)
       override def events(): Observable[Event] = guiJs.events()
   trait Interface extends Provider with Component
