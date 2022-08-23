098cc1c7903c48e0ef3316ce822df4d10355ae65
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-23 10:43:21 +0200

    chore: change type of path in js gui

--- root/js/src/main/scala/it/unibo/pps/js/boundary/JSGUIModule.scala
+++ root/js/src/main/scala/it/unibo/pps/js/boundary/JSGUIModule.scala
@@ -3,6 +3,7 @@ package it.unibo.pps.js.boundary
 import it.unibo.pps.boundary.BoundaryModule.ConfigBoundary
 import it.unibo.pps.boundary.component.Events.Event
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationError
+import it.unibo.pps.control.parser.ReaderModule.FilePath
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
 import monix.eval.Task
 import monix.reactive.Observable
@@ -14,7 +15,7 @@ object JSGUIModule:
     class JSGUIBoundaryImpl extends ConfigBoundary:
       private val guiJs = JSGUI()
       override def init() = guiJs.init()
-      override def config(): Task[String] = guiJs.config()
+      override def config(): Task[FilePath] = guiJs.config()
       override def error(errors: Seq[ConfigurationError]): Task[Unit] = guiJs.error(errors)
       override def start() = Task.pure {}
       override def stop() = Task.pure {}
