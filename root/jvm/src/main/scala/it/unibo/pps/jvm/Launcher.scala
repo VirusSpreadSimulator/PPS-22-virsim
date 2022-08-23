572231c6df0136fce99a16b8981d979411cd44ff
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-23 10:43:54 +0200

    chore: add reader to jvm launcher

--- root/jvm/src/main/scala/it/unibo/pps/jvm/Launcher.scala
+++ root/jvm/src/main/scala/it/unibo/pps/jvm/Launcher.scala
@@ -4,12 +4,18 @@ import it.unibo.pps.boundary.BoundaryModule
 import it.unibo.pps.boundary.BoundaryModule.Boundary
 import it.unibo.pps.boundary.BoundaryModule.ConfigBoundary
 import it.unibo.pps.control.launcher.Launch
-import it.unibo.pps.control.parser.ParserModule
+import it.unibo.pps.control.parser.{ParserModule, ReaderModule}
 import it.unibo.pps.jvm.boundary.GUIModule
 import it.unibo.pps.jvm.boundary.exporter.ExporterModule
 import it.unibo.pps.jvm.parser.ScalaParser
+import it.unibo.pps.jvm.parser.JVMReader
 
-object Launcher extends Launch with GUIModule.Interface with ScalaParser.Interface with ExporterModule.Interface:
+object Launcher
+    extends Launch
+    with GUIModule.Interface
+    with ScalaParser.Interface
+    with ExporterModule.Interface
+    with JVMReader.Interface:
 
   override val gui: ConfigBoundary = GUIBoundaryImpl()
   override val configBoundary: ConfigBoundary = gui
@@ -17,5 +23,7 @@ object Launcher extends Launch with GUIModule.Interface with ScalaParser.Interfa
   override val boundaries: Seq[Boundary] = Seq(gui, exporter)
   override val scalaParser: ParserModule.Parser = ParserImpl()
   override val parser: ParserModule.Parser = scalaParser
+  override val jvmReader: ReaderModule.Reader = JVMReaderImpl()
+  override val reader: ReaderModule.Reader = jvmReader
 
   @main def main(): Unit = launch()
