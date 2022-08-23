8bfcceb3780cf361de75cc50a5282fe0de25f3fb
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-23 14:28:01 +0200

    chore(js): add values

---
+++ root/js/src/main/scala/it/unibo/pps/js/boundary/Values.scala
@@ -0,0 +1,33 @@
+package it.unibo.pps.js.boundary
+
+object Values:
+  object Text:
+    val PAUSE_BTN: String = "Pause"
+    val RESUME_BTN: String = "Resume"
+    val STOP_BTN: String = "Stop"
+    val INVALID_FILE_LABEL: String = "Invalid file "
+    val WRONG_PARAMETER_LABEL: String = "Wrong Parameter "
+    val SWITCH_MASK_OBLIGATION: String = "Switch mask obligation"
+    val VACCINE_ROUND: String = "Vaccine round"
+    val SWITCH_STRUCTURE_OPEN: String = " Switch group "
+
+  object BootstrapClasses:
+    val BTN: String = "btn"
+    val BTN_SECONDARY: String = s"$BTN btn-secondary"
+    val PAUSE_BTN: String = s"$BTN btn-warning"
+    val RESUME_BTN: String = s"$BTN btn-success"
+    val STOP_BTN: String = s"$BTN btn-danger"
+    val MARGIN_TOP_1: String = "mt-1"
+
+  object SimulationColor:
+    val BACKGROUND_COLOR: String = "rgb(56, 142, 60)"
+    val HEALTHY_ENTITY_COLOR: String = "rgb(118, 255, 3)"
+    val INFECTED_ENTITY_COLOR: String = "rgb(221, 44, 0)"
+    val IMMUNITY_COLOR: String = "rgb(13, 71, 161)"
+    val HOUSE_COLOR: String = "rgb(144, 164, 174)"
+    val GENERIC_COLOR_OPEN: String = "rgb(255, 145, 0)"
+    val GENERIC_COLOR_CLOSED: String = "rgb(255, 183, 77)"
+    val HOSPITAL_COLOR: String = "rgb(3, 169, 244)"
+    val VISIBILITY_RANGE_COLOR: String = "rgba(212, 225, 87, 220)"
+    val STRUCTURE_CAPACITY_COLOR: String = "rgb(255, 0, 0)"
+    val WHITE: String = "rgba(255, 255, 255, 0.2)"
