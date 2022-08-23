bc4da41b5150f1cbb7f6d80a09588bc5cdf07c71
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-23 10:41:39 +0200

    feat: add js implementation of reader

---
+++ root/js/src/main/scala/it/unibo/pps/js/boundary/parser/JSReader.scala
@@ -0,0 +1,32 @@
+package it.unibo.pps.js.boundary.parser
+
+import it.unibo.pps.control.loader.configuration.SimulationDefaults.GlobalDefaults
+import it.unibo.pps.control.parser.ReaderModule
+import it.unibo.pps.control.parser.ReaderModule.{FilePath, Reader}
+import org.scalajs.dom.{File, FileReader, UIEvent}
+import monix.eval.Task
+import monix.reactive.Consumer
+import monix.reactive.subjects.PublishSubject
+import org.scalajs.dom
+
+object JSReader:
+
+  trait Provider:
+    val jsReader: Reader
+
+  trait Component:
+    class JSReaderImpl extends Reader:
+
+      private val filePS: PublishSubject[String] = PublishSubject[String]()
+
+      override def read(filePath: FilePath): Task[String] =
+        val reader: FileReader = new FileReader()
+        val file: File = filePath.path.asInstanceOf[File]
+        reader.readAsText(file, "UTF-8")
+        reader.onload = _ => filePS.onNext(s"${reader.result}")
+        Task.defer(filePS.consumeWith(Consumer.head))
+
+  case class DomFile(path: File) extends FilePath:
+    override type Path = File
+
+  trait Interface extends Provider with Component
