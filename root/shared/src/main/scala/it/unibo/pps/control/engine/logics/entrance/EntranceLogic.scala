bc2dcfbd9d49dd20447aabee84544d6dec9530aa
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-23 14:49:51 +0200

    feat: add entrance logic

--- root/shared/src/main/scala/it/unibo/pps/control/engine/logics/entrance/EntranceLogic.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/engine/logics/entrance/EntranceLogic.scala
@@ -4,11 +4,13 @@ import it.unibo.pps.control.engine.logics.Logic.UpdateLogic
 import it.unibo.pps.entity.common.Time.TimeStamp
 import it.unibo.pps.entity.common.Utils.*
 import it.unibo.pps.entity.entity.Entities.SimulationEntity
+import it.unibo.pps.entity.entity.EntityComponent.Moving.MovementGoal
 import it.unibo.pps.entity.entity.EntityComponent.{Infectious, Moving}
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
 import it.unibo.pps.entity.structure.StructureComponent.Visible
-import it.unibo.pps.entity.structure.Structures.SimulationStructure
+import it.unibo.pps.entity.structure.Structures.{House, SimulationStructure}
 import monix.eval.Task
+import monocle.syntax.all.*
 
 class EntranceLogic extends UpdateLogic:
   import it.unibo.pps.entity.common.Utils.*
@@ -16,27 +18,38 @@ class EntranceLogic extends UpdateLogic:
 
   override def apply(environment: Environment): Task[Environment] =
     for
-      entities <- Task(environment.externalEntities)
-      entered <- Task {
-        entities
-          .filter(entity => checkIfIsInARay(entity, environment.structures.select[VisibleStructure]).isDefined)
-          .filter(entity =>
-            tryToEnter(
-              entity,
-              checkIfIsInARay(entity, environment.structures.select[VisibleStructure]).get,
-              environment.time
+      updateEnv <- Task {
+        environment.externalEntities
+          .foldLeft(environment)((env, entity) => tryToEnter(env, entity).getOrElse(env))
+
+      }
+    yield {
+      println("size = " + updateEnv.externalEntities.size)
+      updateEnv
+    }
+
+  private def tryToEnter(environment: Environment, entity: SimulationEntity): Option[Environment] =
+    val oldStruct = environment.structures
+      .select[VisibleStructure]
+      .find(structure => entity.position.distanceTo(structure.position) <= structure.visibilityDistance)
+    if oldStruct.isDefined then
+      val updatedStruct = oldStruct.get.tryToEnter(entity, environment.time)
+      if updatedStruct.entities.map(_.entity).contains(entity) then
+        if updatedStruct.isInstanceOf[House] then
+          println("entity entered in home = " + updatedStruct + " is " + entity.id)
+        Some(
+          environment.update(
+            externalEntities = environment.externalEntities - entity,
+            structures = environment.structures - oldStruct.get + updatedStruct.updateEntitiesInside(entity =>
+              Some(
+                entity
+                  .focus(_.movementGoal)
+                  .replace(MovementGoal.NO_MOVEMENT)
+                  .focus(_.position)
+                  .replace(updatedStruct.position)
+              )
             )
           )
-      }
-    yield environment.update(externalEntities =
-      environment.externalEntities.filter(e => !entered.map(_.id).contains(e.id))
-    )
-
-  private def checkIfIsInARay(
-      entity: SimulationEntity,
-      structures: Set[VisibleStructure]
-  ): Option[VisibleStructure] =
-    structures.find(structure => entity.position.distanceTo(structure.position) <= structure.visibilityDistance)
-
-  private def tryToEnter(entity: SimulationEntity, structure: VisibleStructure, timestamp: TimeStamp): Boolean =
-    structure.tryToEnter(entity, timestamp).entities.map(_.entity).contains(entity)
+        )
+      else None
+    else None
