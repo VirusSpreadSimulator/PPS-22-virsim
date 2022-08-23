943b4bc0b7180c839c84ad56b0b5856897d8583a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-08-23 10:44:09 +0200

    chore: add reader to abstract launcher

--- root/shared/src/main/scala/it/unibo/pps/control/launcher/Launch.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/launcher/Launch.scala
@@ -7,6 +7,7 @@ import it.unibo.pps.control.engine.EngineModule.Engine
 import it.unibo.pps.control.engine.config.EngineConfiguration
 import it.unibo.pps.control.launcher.LauncherModule
 import it.unibo.pps.control.launcher.LauncherModule.Launcher
+import it.unibo.pps.control.parser.ReaderModule
 import it.unibo.pps.control.loader.LoaderModule
 import it.unibo.pps.control.loader.LoaderModule.Loader
 import it.unibo.pps.control.parser.ParserModule
@@ -21,7 +22,8 @@ trait Launch
     with LoaderModule.Interface
     with ParserModule.Interface
     with EngineModule.Interface
-    with EnvironmentModule.Interface:
+    with EnvironmentModule.Interface
+    with ReaderModule.Interface:
 
   import it.unibo.pps.control.engine.config.EngineConfiguration.given
 
