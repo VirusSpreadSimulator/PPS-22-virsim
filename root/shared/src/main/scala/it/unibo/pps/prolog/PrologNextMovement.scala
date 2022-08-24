b15fc48493378a90d06a7ff374ac6d18e9ced036
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-24 14:37:40 +0200

    chore: delete some commented code

---
+++ root/shared/src/main/scala/it/unibo/pps/prolog/PrologNextMovement.scala
@@ -0,0 +1,72 @@
+package it.unibo.pps.prolog
+
+import it.unibo.pps.entity.common.Space.Point2D
+import it.unibo.pps.prolog.PrologEngine.{*, given}
+
+object PrologNextMovement:
+  val newX: Var = Var("Xn")
+  val newY: Var = Var("Yn")
+
+  /** Calculates all possible next movement based on current position
+    * @param currPosition
+    *   the current position of the entity
+    * @param worldWidth
+    *   the width of the world
+    * @param worldHeight
+    *   the height of the world
+    * @param step
+    *   the step of the movement
+    * @return
+    *   a set of possible next position
+    */
+  def calculateNextMovement(currPosition: Point2D, worldWidth: Int, worldHeight: Int, step: Int): Set[Point2D] =
+    val currentPosition = s"point(${currPosition._1}, ${currPosition._2})"
+    val input = Struct(
+      "newPoint",
+      Term.createTerm(currentPosition),
+      newX,
+      newY,
+      -step to step,
+      Term.createTerm(worldWidth.toString),
+      Term.createTerm(worldHeight.toString)
+    )
+    engine(input)
+      .map(a => Point2D(extractTerm(a, 1).toString.toInt, extractTerm(a, 2).toString.toInt))
+      .toSet
+
+  /** Calculates all possible next movement for entity that has to return to home
+    * @param currPosition
+    *   the current position of the entity
+    * @param worldWidth
+    *   the width of the world
+    * @param worldHeight
+    *   the height of the world
+    * @param step
+    *   the step of the movement
+    * @param homePos
+    *   the position of the home
+    * @return
+    *   a set of possible next position
+    */
+  def calculateNextMovementToGoHome(
+      currPosition: Point2D,
+      worldWidth: Int,
+      worldHeight: Int,
+      step: Int,
+      homePos: Point2D
+  ): Set[Point2D] =
+    val currentPosition = s"point(${currPosition._1}, ${currPosition._2})"
+    val homePosition = s"home(${homePos._1}, ${homePos._2})"
+    val input = Struct(
+      "goHome",
+      Term.createTerm(currentPosition),
+      newX,
+      newY,
+      -step to step,
+      Term.createTerm(worldWidth.toString),
+      Term.createTerm(worldHeight.toString),
+      Term.createTerm(homePosition)
+    )
+    engine(input)
+      .map(a => Point2D(extractTerm(a, 1).toString.toInt, extractTerm(a, 2).toString.toInt))
+      .toSet
