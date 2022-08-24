283758ebb691c2d7b447e8952ce0120a162ba0b2
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-24 22:39:15 +0200

    test: added test on fields

--- root/shared/src/test/scala/it/unibo/pps/entity/structure/entrance/PermanenceTests.scala
+++ root/shared/src/test/scala/it/unibo/pps/entity/structure/entrance/PermanenceTests.scala
@@ -7,7 +7,6 @@ import it.unibo.pps.entity.structure.entrance.Permanence.EntityPermanence
 import it.unibo.pps.entity.structure.entrance.Permanence.PermanenceStatus
 import it.unibo.pps.entity.entity.Entities.SimulationEntity
 import it.unibo.pps.entity.structure.Structures.House
-
 import scala.concurrent.duration.MINUTES
 
 class PermanenceTests extends AnyFunSuite with Matchers:
@@ -18,6 +17,18 @@ class PermanenceTests extends AnyFunSuite with Matchers:
   private val permanenceValid = DurationTime(1, MINUTES)
   private val permanenceOver = DurationTime(11, MINUTES)
   private val permanence = EntityPermanence(entity, timestamp, permanenceDuration)
+
+  test("It is possible to obtain the entity of the permanence") {
+    permanence.entity shouldBe entity
+  }
+
+  test("It is possible to obtain the timestamp of the permanence") {
+    permanence.timestamp shouldBe timestamp
+  }
+
+  test("It is possible to obtain the duration of the permanence") {
+    permanence.permanenceDuration shouldBe permanenceDuration
+  }
 
   test("Permanence should be able to understand when its over, valid case") {
     val timeStampValid = timestamp + permanenceValid
