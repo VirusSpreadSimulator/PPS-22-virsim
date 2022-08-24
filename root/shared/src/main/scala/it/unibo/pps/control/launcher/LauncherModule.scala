31b22194835bb1e2d1cbc95464245941e238e660
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-24 18:44:21 +0200

    docs: improve documentation

--- root/shared/src/main/scala/it/unibo/pps/control/launcher/LauncherModule.scala
+++ root/shared/src/main/scala/it/unibo/pps/control/launcher/LauncherModule.scala
@@ -2,20 +2,29 @@ package it.unibo.pps.control.launcher
 
 import it.unibo.pps.boundary.BoundaryModule
 import it.unibo.pps.control.loader.LoaderModule
-import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationResult
 import it.unibo.pps.control.loader.configuration.ConfigurationComponent.ConfigurationResult.*
-import it.unibo.pps.control.loader.configuration.ConfigurationComponent.given
-
 import monix.eval.Task
 
 object LauncherModule:
+  /** The launcher is the component that is responsible for the launch of the simulation. */
   trait Launcher:
+    /** Method that function as entry point for all the flow.
+      * @return
+      *   the task
+      */
     def launch(): Task[Unit]
+  /** Provider of the component. */
   trait Provider:
     val launcher: Launcher
+  /** The launcher require:
+    *   - the loader in order to launch the loading of the user configuration and of the environment
+    *   - the boundaries in order to be able to signal errors and the start of the simulation.
+    */
   type Requirements = BoundaryModule.Provider with LoaderModule.Provider
+  /** Launcher component. */
   trait Component:
     context: Requirements =>
+    /** Implementation of the launcher component. */
     class LauncherImpl extends Launcher:
 
       override def launch(): Task[Unit] =
@@ -24,6 +33,7 @@ object LauncherModule:
           _ <- launcherLoop()
         yield ()
 
+      /** Method that is responsible to loop until the user submit a correct configuration that can be loaded. */
       private def launcherLoop(): Task[Unit] =
         for
           path <- context.configBoundary.config()
@@ -40,6 +50,6 @@ object LauncherModule:
                 _ <- context.loader.startEngine(configuration)
               yield ()
         yield ()
-
+  /** The interface of the component. */
   trait Interface extends Provider with Component:
     self: Requirements =>
