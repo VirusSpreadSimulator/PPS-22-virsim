9787ce696ebac861099ecd9e40c0a77c45608bc6
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-24 19:16:37 +0200

    refactor: refactor gaussian property

--- root/shared/src/main/scala/it/unibo/pps/entity/common/GaussianProperty.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/common/GaussianProperty.scala
@@ -10,38 +10,45 @@ object GaussianProperty:
     *   the type of the Property
     */
   trait Gaussian[A]:
-    /** The mean of the gaussian distribution
+    /** The mean of the gaussian distribution.
       * @return
       *   the mean
       */
     def mean: Double
-    /** The standard deviation of the gaussian distribution
+    /** The standard deviation of the gaussian distribution.
       * @return
       *   the standard deviation
       */
     def std: Double
-    /** Method that allow to generate the next gaussian that follow the distribution
-      * @return
-      *   the generated number
-      */
-    protected def nextGaussian(): Double = std * Random.nextGaussian() + mean
     /** Method that allow to generate the next value for the property of type [[A]].
       * @return
       *   the generated property value
       */
-    def next(): A
+    def next(): A = convert(nextGaussian())
+    /** Method to implement the conversion from the generated number to the required gaussian type [[A]] */
+    protected def convert(next: Double): A
+    /** Method that allow to generate the next gaussian that follow the distribution.
+      * @return
+      *   the generated number
+      */
+    private def nextGaussian(): Double = std * Random.nextGaussian() + mean
 
-  /** A duration time generator that follow a gaussian distribution
+  /** A duration time generator that follow a gaussian distribution.
     * @param mean
     *   the mean of the gaussian distribution
     * @param std
     *   the standard deviation of the gaussian distribution
     * @param unit
-    *   the unit choosen for the duration time object
+    *   the unit chosen for the duration time object
     */
-  class GaussianDurationTime(override val mean: Double, override val std: Double, unit: TimeUnit)
-      extends Gaussian[DurationTime]:
-    override def next(): DurationTime = DurationTime(nextGaussian().toLong, unit)
+  case class GaussianDurationTime(mean: Double, val std: Double, unit: TimeUnit) extends Gaussian[DurationTime]:
+    override protected def convert(next: Double): DurationTime = DurationTime(next.toLong, unit)
 
-  class GaussianIntDistribution(override val mean: Double, override val std: Double) extends Gaussian[Int]:
-    override def next(): Int = nextGaussian().toInt
+  /** A integer generator that follow a gaussian distribution.
+    * @param mean
+    *   the mean of the gaussian distribution
+    * @param std
+    *   the standard deviation of the gaussian distribution
+    */
+  case class GaussianIntDistribution(mean: Double, std: Double) extends Gaussian[Int]:
+    override protected def convert(next: Double): Int = next.toInt
