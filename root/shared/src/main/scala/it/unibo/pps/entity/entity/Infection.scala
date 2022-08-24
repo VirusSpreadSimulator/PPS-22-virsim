1acc51c09f52bdde43ff0cdc2e1a8c4ab2191ff8
giacomo accursi <giacomo.accursi@gmail.com>
2022-08-24 11:15:39 +0200

    chore: improve docs

--- root/shared/src/main/scala/it/unibo/pps/entity/entity/Infection.scala
+++ root/shared/src/main/scala/it/unibo/pps/entity/entity/Infection.scala
@@ -3,9 +3,25 @@ package it.unibo.pps.entity.entity
 import it.unibo.pps.entity.common.Time.{DurationTime, TimeStamp}
 import it.unibo.pps.entity.entity.Infection.Severity
 
+/*Represent the infection of an entity*/
 trait Infection:
+
+  /** Represent the severity of the infection.
+    * @return
+    *   the severity of the infection
+    */
   def severity: Severity
+
+  /** Represents when the infection occurred.
+    * @return
+    *   the timestamp of when the infection occurred
+    */
   def timeOfTheInfection: TimeStamp
+
+  /** Represent the duration of the infection.
+    * @return
+    *   the duration of the infection
+    */
   def duration: DurationTime
 
 object Infection:
@@ -17,6 +33,15 @@ object Infection:
       case SERIOUS() => 1.8
       case _ => 1.2
 
+  /** Factory for infection
+    * @param severity
+    *   the severity of the infection
+    * @param time
+    *   the time when infection occurred
+    * @param duration
+    *   the duration of the infection
+    * @return
+    */
   def apply(severity: Severity, time: TimeStamp, duration: DurationTime): Infection =
     InfectionImpl(severity, time, duration)
   private class InfectionImpl(
