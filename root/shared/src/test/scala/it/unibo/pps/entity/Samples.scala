779b612cfa4d873fbcf855ea1d9a61e15eff4213
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-25 10:09:50 +0200

    refactor: remove magic numbers

--- root/shared/src/test/scala/it/unibo/pps/entity/Samples.scala
+++ root/shared/src/test/scala/it/unibo/pps/entity/Samples.scala
@@ -6,15 +6,42 @@ import it.unibo.pps.entity.entity.Infection.Severity
 import it.unibo.pps.entity.structure.Structures.{GenericBuilding, Hospital, House, SimulationStructure}
 import it.unibo.pps.entity.common.Time.{DurationTime, TimeStamp}
 import it.unibo.pps.entity.entity.Entities.SimulationEntity
-import it.unibo.pps.entity.entity.EntityComponent.Entity
 import it.unibo.pps.entity.environment.EnvironmentModule
 import it.unibo.pps.entity.environment.EnvironmentModule.Environment
 import it.unibo.pps.entity.structure.entrance.Permanence.EntityPermanence
-
 import scala.concurrent.duration.{DAYS, MINUTES}
 
+/** Module that contains some simple samples useful during testing. */
 object Samples:
-  private val house = House((1, 0), 1, 2)
+  private val age = 20
+  private val immunity = 70
+  private val health = 80
+  private val atRiskHealth = 0.001
+  private val infectionProbability = 0.5
+  private val capacity = 4
+  private val durationLong = DurationTime(11, MINUTES)
+  private val durationShort = DurationTime(0, MINUTES)
+  private val house = House(Point2D(1, 0), infectionProbability, capacity)
+  /** Method to create a permanence for an entity created on the fly.
+    * @param id
+    *   the id of the entity
+    * @param age
+    *   the age of the entity
+    * @param housePosition
+    *   the house position of the entity
+    * @param health
+    *   the health of the entity
+    * @param position
+    *   the position of the entity
+    * @param infectionDuration
+    *   the infection duration, an [[Option]] if [[None]] then there is no infection
+    * @param immunity
+    *   the immunity of the entity
+    * @param duration
+    *   the duration of the permanence
+    * @return
+    *   the [[EntityPermanence]]
+    */
   private def permanence(
       id: Int,
       age: Int,
@@ -23,7 +50,7 @@ object Samples:
       position: Point2D,
       infectionDuration: Option[DurationTime] = None,
       immunity: Double = 0,
-      duration: DurationTime = DurationTime(11, MINUTES)
+      duration: DurationTime = durationLong
   ) =
     EntityPermanence(
       SimulationEntity(
@@ -42,83 +69,83 @@ object Samples:
     )
 
   private val entities: Set[SimulationEntity] = Set(
-    SimulationEntity(5, 20, house.position, 80, 10, position = Point2D(1, 2)),
-    SimulationEntity(6, 21, house.position, 80, 10, position = Point2D(3, 2)),
-    SimulationEntity(7, 22, house.position, 80, 10, position = Point2D(4, 5)),
+    SimulationEntity(5, age, house.position, health, immunity, position = Point2D(1, 2)),
+    SimulationEntity(6, age, house.position, health, immunity, position = Point2D(3, 2)),
+    SimulationEntity(7, age, house.position, health, immunity, position = Point2D(4, 5)),
     SimulationEntity(
       8,
-      10,
+      age,
       house.position,
-      0.001,
-      70,
+      atRiskHealth,
+      immunity,
       position = Point2D(4, 2),
-      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), DurationTime(0, MINUTES)))
+      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), durationLong))
     ),
     SimulationEntity(
       9,
-      20,
+      age,
       house.position,
-      80,
-      20,
+      health,
+      immunity,
       position = Point2D(1, 1),
-      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), DurationTime(5, DAYS)))
+      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), durationLong))
     ),
     SimulationEntity(
       10,
-      20,
+      age,
       house.position,
-      80,
-      10,
+      health,
+      immunity,
       position = Point2D(1, 1),
-      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), DurationTime(0, MINUTES)))
+      infection = Some(Infection(Severity.LIGHT(), TimeStamp(), durationShort))
     )
   )
   private val buildings: Set[SimulationStructure] = Set(
     GenericBuilding(
       Point2D(1, 7),
-      0.5,
-      4,
+      infectionProbability,
+      capacity,
       entities = Set(
-        permanence(11, 20, house.position, 80, Point2D(1, 7)),
-        permanence(12, 10, house.position, 0.001, Point2D(1, 7), Some(DurationTime(5, DAYS))),
-        permanence(13, 20, house.position, 80, Point2D(1, 7), Some(DurationTime(5, DAYS))),
-        permanence(14, 20, house.position, 0.001, Point2D(1, 7), Some(DurationTime(0, MINUTES)))
+        permanence(11, age, house.position, health, Point2D(1, 7)),
+        permanence(12, age, house.position, atRiskHealth, Point2D(1, 7), Some(durationLong)),
+        permanence(13, age, house.position, health, Point2D(1, 7), Some(durationLong)),
+        permanence(14, age, house.position, atRiskHealth, Point2D(1, 7), Some(durationShort))
       )
     ),
     GenericBuilding(
       Point2D(8, 7),
-      0.5,
-      4,
+      infectionProbability,
+      capacity,
       entities = Set(
-        permanence(15, 21, house.position, 80, Point2D(8, 7), None, 10, DurationTime(0, MINUTES)),
-        permanence(16, 21, house.position, 80, Point2D(8, 7), None, 20, DurationTime(11, MINUTES))
+        permanence(15, age, house.position, health, Point2D(8, 7), None, 10, durationShort),
+        permanence(16, age, house.position, health, Point2D(8, 7), None, 20, durationLong)
       )
     ),
-    GenericBuilding(
-      Point2D(4, 5),
-      0.3,
-      4,
-      group = "group1"
-    ),
-    GenericBuilding(
-      Point2D(1, 5),
-      0.3,
-      4,
-      group = "group1"
-    )
+    GenericBuilding(Point2D(4, 5), infectionProbability, capacity, group = "group1"),
+    GenericBuilding(Point2D(1, 5), infectionProbability, capacity, group = "group1")
   )
 
+  /** A set of [[EntityPermanence]] with some infected entities. */
   val genericInfectedPermanences: Set[EntityPermanence] = Set(
-    permanence(1, 20, house.position, 80, Point2D(1, 7)),
-    permanence(2, 10, house.position, 0.001, Point2D(1, 7), Some(DurationTime(5, DAYS))),
-    permanence(3, 20, house.position, 80, Point2D(1, 7), Some(DurationTime(5, DAYS))),
-    permanence(4, 20, house.position, 80, Point2D(1, 7), Some(DurationTime(0, MINUTES)))
+    permanence(1, age, house.position, health, Point2D(1, 7)),
+    permanence(2, age, house.position, atRiskHealth, Point2D(1, 7), Some(durationLong)),
+    permanence(3, age, house.position, health, Point2D(1, 7), Some(durationLong)),
+    permanence(4, age, house.position, health, Point2D(1, 7), Some(durationShort))
   )
-  val inhabitatedHouse: House = House((1, 0), 1, 2, entities = genericInfectedPermanences)
+  /** An [[House]] with some [[EntityPermanence]]. Some of them are infected. */
+  val inhabitatedHouse: House =
+    House(Point2D(1, 0), infectionProbability, capacity, entities = genericInfectedPermanences)
+  /** A [[GenericBuilding]] with some [[EntityPermanence]]. Some of them are infected. */
   val inhabitatedGenericBuilding: GenericBuilding =
-    GenericBuilding(Point2D(8, 7), 0.5, 4, entities = genericInfectedPermanences)
-  val inhabitatedHospital: Hospital = Hospital((1, 0), 1, 2, entities = genericInfectedPermanences)
+    GenericBuilding(Point2D(8, 7), infectionProbability, capacity, entities = genericInfectedPermanences)
+  /** An [[Hospital]] with some [[EntityPermanence]]. Some of them are infected. */
+  val inhabitatedHospital: Hospital =
+    Hospital((1, 0), infectionProbability, capacity, entities = genericInfectedPermanences)
 
+  /** Method to create a sample env, with some infected entity inside.
+    * @return
+    *   the environment
+    */
   def sampleEnv: Environment =
     object InfectedEnv extends EnvironmentModule.Interface:
       val env: Environment = EnvironmentImpl(externalEntities = entities, structures = buildings)
