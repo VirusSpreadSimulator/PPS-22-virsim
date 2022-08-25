c047c6accd680132f50b15790c87416eef6fdf12
Andrea Giulianelli <andrygiulio@gmail.com>
2022-08-25 23:28:30 +0200

    refactor: refactor simulation canvas

--- root/js/src/main/scala/it/unibo/pps/js/boundary/gui/panel/SimulationCanvas.scala
+++ root/js/src/main/scala/it/unibo/pps/js/boundary/gui/panel/SimulationCanvas.scala
@@ -7,14 +7,15 @@ import it.unibo.pps.boundary.component.panel.Panels.UpdatablePanel
 import monix.eval.Task
 import org.scalajs.dom
 
+/** The Simulation Canvas is the panel that handle the visualization of the simulation status. */
 class SimulationCanvas extends UpdatablePanel:
-  private val canvas = dom.document.createElement("canvas").asInstanceOf[dom.html.Canvas]
-  private val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+  private lazy val canvas = dom.document.createElement("canvas").asInstanceOf[dom.html.Canvas]
+  private lazy val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
   override def init(): Task[Unit] = for
     _ <- io(dom.document.getElementById("simulation").appendChild(canvas))
     _ <- io(setCanvasSize())
-    _ <- io(dom.window.onresize = e => setCanvasSize())
+    _ <- io(dom.window.onresize = _ => setCanvasSize())
   yield ()
 
   override def update(newEnv: Environment): Task[Unit] =
